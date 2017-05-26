a0cce9604edfb26434e91597c0812803fd75b1d6
peaBerberian <pea.berberian@gmail.com>
2017-05-26 16:10:11 +0200

    utils: move object assign polyfill in utils

--- src/index.js
+++ src/index.js
@@ -16,24 +16,7 @@
 
 if (typeof Object.assign != "function") {
   (function () {
-    Object.assign = function(target) {
-      if (target === undefined || target === null) {
-        throw new TypeError("Cannot convert undefined or null to object");
-      }
-
-      const output = Object(target);
-      for (let index = 1; index < arguments.length; index++) {
-        const source = arguments[index];
-        if (source !== undefined && source !== null) {
-          for (const nextKey in source) {
-            if (source.hasOwnProperty(nextKey)) {
-              output[nextKey] = source[nextKey];
-            }
-          }
-        }
-      }
-      return output;
-    };
+    Object.assign = require("./utils/object-assign.js");
   })();
 }
 
