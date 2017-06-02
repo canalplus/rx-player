d531ac09d73260bb7bcd16804d5d21642a722b7f
peaBerberian <pea.berberian@gmail.com>
2017-06-02 12:51:47 +0200

    demo: fix dependencies

--- demo/scripts/index.js
+++ demo/scripts/index.js
@@ -12,8 +12,10 @@ import "rxjs/add/operator/do";
 import "rxjs/add/operator/filter";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/skip";
 import "rxjs/add/operator/startWith";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/take";
+import "rxjs/add/operator/takeUntil";
 
 ReactDOM.render(<Main />, document.getElementById("player-container"));
