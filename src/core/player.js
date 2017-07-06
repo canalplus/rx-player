0b26315f6c634025d26afd42ef241b2437a764d4
peaBerberian <pea.berberian@gmail.com>
2017-07-06 17:44:48 +0200

    update version

--- src/core/player.js
+++ src/core/player.js
@@ -281,7 +281,7 @@ class Player extends EventEmitter {
     // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1194624
     videoElement.preload = "auto";
 
-    this.version = /*PLAYER_VERSION*/"2.2.1";
+    this.version = /*PLAYER_VERSION*/"2.3.0";
     this.video = videoElement;
 
     // fullscreen change subscription.
