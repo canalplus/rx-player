a26b9cf463de3d8eab9c2a86d218cff0c874f975
peaBerberian <pea.berberian@gmail.com>
2017-08-02 18:29:52 +0200

    compat: add isOffline method

--- src/compat/index.js
+++ src/compat/index.js
@@ -184,6 +184,32 @@ function clearVideoSrc(video) {
   video.removeAttribute("src");
 }
 
+/**
+ * Some browsers have a builtin API to know if it's connected at least to a
+ * LAN network, at most to the internet.
+ *
+ * /!\ This feature can be dangerous as you can both have false positives and
+ * false negatives.
+ *
+ * False positives:
+ *   - you can still play local contents (on localhost) if isOffline == true
+ *   - on some browsers isOffline might be true even if we're connected to a LAN
+ *     or a router (it would mean we're just not able to connect to the
+ *     Internet). So we can eventually play LAN contents if isOffline == true
+ *
+ * False negatives:
+ *   - in some cases, we even might have isOffline at false when we do not have
+ *     any connection:
+ *       - in browsers that do not support the feature
+ *       - in browsers running in some virtualization softwares
+ *
+ * Use with these cases in mind.
+ * @returns {Boolean}
+ */
+function isOffline() {
+  return navigator.onLine === false;
+}
+
 export {
   HTMLVideoElement_,
   KeySystemAccess,
@@ -198,6 +224,7 @@ export {
   isFirefox,
   isFullscreen,
   isIE,
+  isOffline,
   isPlaybackStuck,
   isVTTSupported,
   requestFullscreen,
