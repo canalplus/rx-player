74e7bae0288e47b4d5281e7700ee540b104bc0ae
peaBerberian <pea.berberian@gmail.com>
2017-09-11 15:01:00 +0200

    demo: remove deprecated code

--- demo/full/scripts/modules/player/events.js
+++ demo/full/scripts/modules/player/events.js
@@ -106,20 +106,6 @@ const linkPlayerEventsToState = (player, state, $destroy) => {
       state.set(stateUpdates);
     });
 
-  // /!\ deprecated
-  // player.getMetrics()
-  //   .takeUntil($destroy)
-  //   // .map((metric) => metric.value.response)
-  //   // .filter((response) => response.size > 2000)
-  //   .subscribe((metric = {}) => {
-  //     const { response } = metric.value;
-  //     if (response && response.size > 10000) {
-  //       state.set({
-  //         bandwidth: (response.size / response.duration) * 0.008, // in mbps
-  //       });
-  //     }
-  //   });
-
   fromPlayerEvent("manifestChange")
     .map(() => player.isLive())
     .distinctUntilChanged()
