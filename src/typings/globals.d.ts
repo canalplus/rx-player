44fe644101f232ed4249cdc66b18f9e97a249686
grenault73 <grenault73@gmail.com>
2017-10-16 18:03:31 +0200

    abr

--- src/typings/globals.d.ts
+++ src/typings/globals.d.ts
@@ -19,3 +19,50 @@ declare const __FEATURES__ : {
 interface IDictionary<T> {
   [keyName : string] : T;
 }
+<<<<<<< HEAD
+=======
+
+interface HTMLVideoElement {
+  webkitGenerateKeyRequest? : (key: string, initData : ArrayBuffer) => void;
+}
+interface HTMLMediaElement {
+  webkitGenerateKeyRequest? : (key: string, initData : ArrayBuffer) => void;
+}
+
+// typescript does not seem to know that one
+// TODO Complete documentation
+// TODO open a ticket on their side?
+// declare class VTTCue {
+//   id: string;
+//   startTime : number;
+//   endTime : number;
+//   size : number;
+//   vertical : string;
+//   align : string;
+//   position : string;
+//   positionAlign : string;
+//   lineAlign : string;
+//   constructor(start : number, end : number, cueText : string);
+// }
+declare class VTTCue {
+  align : string;
+  endTime : number;
+  id : string;
+  line : string;
+  lineAlign : string;
+  position : number|string;
+  positionAlign : string;
+  size : number|string;
+  snapToLines : boolean;
+  startTime : number;
+  vertical : string;
+  constructor(start : number, end : number, cueText : string);
+}
+
+// XXX TODO understand how I'm supposed to do that
+declare function arrayFind<T>(arr : T[], predicate : (arg: T) => boolean) : T;
+
+interface Dictionary<T> {
+  [Key: string]: T;
+}
+>>>>>>> 3930e26... abr
