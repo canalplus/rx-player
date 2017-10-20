1e5b1639840159d6d89a6e2c5425ab760458a784
peaBerberian <pea.berberian@gmail.com>
2017-10-20 16:14:15 +0200

    languages/vtt: fix regexp

--- src/parsers/texttracks/webvtt/native.js
+++ src/parsers/texttracks/webvtt/native.js
@@ -1,5 +1,21 @@
-import arrayIncludes from "../../../../utils/array-includes.js";
-import { makeCue } from "../../../../compat/index.js";
+/**
+ * Copyright 2015 CANAL+ Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import arrayIncludes from "../../../utils/array-includes.js";
+import { makeCue } from "../../../compat/index.js";
 
 // Simple VTT to VTTCue parser:
 // Just parse cues and associated settings.
@@ -12,11 +28,8 @@ import { makeCue } from "../../../../compat/index.js";
  * @returns {Array.<VTTCue|TextTrackCue>}
  */
 export default function parseVTTStringToVTTCues(vttStr) {
-  // TODO Take every block but STYLE REGION NOTE or the first line
-  // Give them to parseCue to construct array
-
   // WEBVTT authorize CRLF, LF or CR as line terminators
-  const lines = vttStr.split(/(\r\n)|\n|\r/);
+  const lines = vttStr.split(/\r\n|\n|\r/);
 
   if (!(/^WEBVTT($ | |\t)/.test(lines[0]))) {
     throw new Error("Can't parse WebVTT: Invalid file.");
@@ -32,8 +45,17 @@ export default function parseVTTStringToVTTCues(vttStr) {
       // continue incrementing i until either:
       //   - empty line
       //   - end
-      for (; lines[i] && i < lines.length; i++) {}
+      while (lines[i]) {
+        i++;
+      }
       cueBlocks.push(lines.slice(startingI, i));
+    } else if (lines[i]) {
+      // continue incrementing i until either:
+      //   - empty line
+      //   - end
+      while (lines[i]) {
+        i++;
+      }
     }
   }
 
