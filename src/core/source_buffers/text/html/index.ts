446e7cdedae715c5505c3c1abc6d619bb2c65bb2
peaBerberian <pea.berberian@gmail.com>
2018-01-18 11:55:50 +0100

    source-buffer: make buffer management more generic

--- src/core/source_buffers/text/html/index.ts
+++ src/core/source_buffers/text/html/index.ts
@@ -24,7 +24,7 @@ import {
 import config from "../../../../config";
 import log from "../../../../utils/log";
 import AbstractSourceBuffer from "../../abstract_source_buffer";
-import TextBufferManager from "./buffer_manager";
+import TimedDataBufferManager from "../../utils/buffer_manager";
 import parseTextTrackToElements from "./parsers";
 
 export interface IHTMLTextTrackData {
@@ -91,7 +91,7 @@ export default class HTMLTextTrackSourceBuffer
   private _destroy$ : Subject<void>;
   private _textTrackElement : HTMLElement;
 
-  private _buffer : TextBufferManager;
+  private _buffer : TimedDataBufferManager<HTMLElement>;
   private _currentElement : HTMLElement|null;
 
   /**
@@ -107,7 +107,7 @@ export default class HTMLTextTrackSourceBuffer
     this._videoElement = videoElement;
     this._textTrackElement = textTrackElement;
     this._destroy$ = new Subject();
-    this._buffer = new TextBufferManager();
+    this._buffer = new TimedDataBufferManager<HTMLElement>();
     this._currentElement = null;
 
     generateClock(this._videoElement)
@@ -129,11 +129,11 @@ export default class HTMLTextTrackSourceBuffer
           safelyRemoveChild(textTrackElement, this._currentElement);
           this._currentElement = null;
           return;
-        } else if (this._currentElement === cue.element) {
+        } else if (this._currentElement === cue.data) {
           return;
         }
         safelyRemoveChild(textTrackElement, this._currentElement);
-        this._currentElement = cue.element;
+        this._currentElement = cue.data;
         textTrackElement.appendChild(this._currentElement);
       });
   }
@@ -176,7 +176,14 @@ export default class HTMLTextTrackSourceBuffer
       type, dataString, timeOffset, language);
     const start = startTime;
     const end = endTime != null ? endTime : cues[cues.length - 1].end;
-    this._buffer.insert(cues, start, end);
+
+    // TODO define "element" as "data" from the beginning?
+    const formattedData = cues.map((cue) => ({
+      start: cue.start,
+      end: cue.end,
+      data: cue.element,
+    }));
+    this._buffer.insert(formattedData, start, end);
     this.buffered.insert(start, end);
   }
 
