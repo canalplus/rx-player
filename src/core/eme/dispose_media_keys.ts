97d5a51460fdd39c48c2383c2b0e5302d3674c99
peaBerberian <pea.berberian@gmail.com>
2018-05-07 19:11:42 +0200

    eme: remove unnecessary operators

--- src/core/eme/dispose_media_keys.ts
+++ src/core/eme/dispose_media_keys.ts
@@ -37,8 +37,6 @@ export default function disposeMediaKeys(
     } = currentState;
     mediaKeysInfos.clearState();
     return sessionsStore.closeAllSessions()
-      .mergeMapTo(setMediaKeys(mediaElement, null))
-      .ignoreElements()
-      .concat(Observable.of(null));
+      .mergeMapTo(setMediaKeys(mediaElement, null));
   });
 }
