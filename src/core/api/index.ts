f6dcf8e7ba5c088712c93e5c46065fc1a2e287ff
peaBerberian <pea.berberian@gmail.com>
2018-05-28 14:46:06 +0200

    update version

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -486,7 +486,7 @@ class Player extends EventEmitter<PLAYER_EVENT_STRINGS, any> {
     // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1194624
     videoElement.preload = "auto";
 
-    this.version = /*PLAYER_VERSION*/"3.4.0";
+    this.version = /*PLAYER_VERSION*/"3.4.1";
     this.log = log;
     this.state = "STOPPED";
     this.videoElement = videoElement;
