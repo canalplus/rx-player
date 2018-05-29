c1e1838de6774945461dea332a882e001b193a9a
peaBerberian <pea.berberian@gmail.com>
2018-05-29 14:26:11 +0200

    net: use redirected URL as base for redirected manifest requests

--- src/net/smooth/index.ts
+++ src/net/smooth/index.ts
@@ -124,16 +124,14 @@ export default function(
     },
 
     parser(
-      { response, url } : IManifestParserArguments<Document|string>
+      { response, url: reqURL } : IManifestParserArguments<Document|string>
     ) : IManifestParserObservable {
+      const url = response.url == null ? reqURL : response.url;
       const data = typeof response.responseData === "string" ?
         new DOMParser().parseFromString(response.responseData, "text/xml") :
         response.responseData;
       const manifest = smoothManifestParser(data, url);
-      return Observable.of({
-        manifest,
-        url: response.url,
-      });
+      return Observable.of({ manifest, url });
     },
   };
 
