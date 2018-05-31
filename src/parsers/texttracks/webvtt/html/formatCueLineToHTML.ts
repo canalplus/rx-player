7adcab64dd55d01071cc5b5ddefa3ec767becc5e
peaBerberian <pea.berberian@gmail.com>
2018-05-31 11:41:24 +0200

    vtt: update style

--- src/parsers/texttracks/webvtt/html/formatCueLineToHTML.ts
+++ src/parsers/texttracks/webvtt/html/formatCueLineToHTML.ts
@@ -18,6 +18,75 @@ import arrayIncludes from "../../../../utils/array-includes";
 import { IStyleElement } from "./parseStyleBlock";
 
 /**
+ * Construct an HTMLElement/TextNode representing the given node and apply
+ * the right styling on it.
+ * @param {Node} baseNode
+ * @param {Array.<Object>} styleElements
+ * @param {Array.<string>} styleClasses
+ * @returns {Node}
+ */
+function createStyledElement(
+  baseNode : Node,
+  styleElements : IStyleElement[],
+  styleClasses : string[]
+) : HTMLElement|Text {
+  const HTMLTags = ["u", "i", "b"];
+  const authorizedNodeNames = ["u", "i", "b", "c", "#text"];
+
+  const mainNodeName = baseNode.nodeName.toLowerCase().split(".")[0];
+  let nodeWithStyle;
+  if (arrayIncludes(authorizedNodeNames, mainNodeName)) {
+    if (mainNodeName === "#text") {
+      nodeWithStyle = document.createTextNode((baseNode as Text).wholeText);
+    } else {
+      const nodeClasses = baseNode.nodeName.toLowerCase().split(".");
+      const classIndexes : number[] = [];
+      nodeClasses.forEach(nodeClass => {
+        if (styleClasses.indexOf(nodeClass) !== -1) {
+          classIndexes.push(styleClasses.indexOf(nodeClass));
+        }
+      });
+      if (classIndexes.length !== 0) { // If style must be applied
+        const attr = document.createAttribute("style");
+        classIndexes.forEach(index => {
+          if (styleElements[index]) {
+            attr.value += styleElements[index].styleContent;
+          }
+        });
+        const nameClass = arrayIncludes(HTMLTags, mainNodeName) ?
+          mainNodeName : "span";
+        nodeWithStyle = document.createElement(nameClass);
+        nodeWithStyle.setAttributeNode(attr);
+      } else { // If style mustn't be applied. Rebuild element with tag name
+        const elementTag = !arrayIncludes(HTMLTags, mainNodeName) ?
+          "span" : mainNodeName;
+        nodeWithStyle = document.createElement(elementTag);
+      }
+      for (let j = 0; j < baseNode.childNodes.length; j++) {
+        const child = createStyledElement(
+          baseNode.childNodes[j],
+          styleElements,
+          styleClasses
+        );
+        nodeWithStyle.appendChild(child);
+      }
+    }
+  } else {
+    nodeWithStyle = document.createElement("span");
+    for (let j = 0; j < baseNode.childNodes.length; j++) {
+      const child = createStyledElement(
+        baseNode.childNodes[j],
+        styleElements,
+        styleClasses
+      );
+      nodeWithStyle.appendChild(child);
+    }
+  }
+
+  return nodeWithStyle;
+}
+
+/**
  * Format WebVTT tags and classes into usual HTML.
  * <b *> => <b>
  * <u *> => <u>
@@ -32,89 +101,25 @@ export default function formatCueLineToHTML(
   text : string,
   styleElements : IStyleElement[]
 ) : Array<HTMLElement|Text> {
-  const HTMLTags = ["u", "i", "b"];
-  const webVTTTags = ["u", "i", "b", "c", "#text"];
-  const styleClasses =
-    styleElements.map(styleElement => styleElement.className);
-  const filtered = text
+
+  const styleClasses = styleElements
+    .map(styleElement => styleElement.className)
+    .filter((className) : className is string => className != null);
+
+  const filteredText = text
     // Remove timestamp tags
     .replace(/<[0-9]{2}:[0-9]{2}.[0-9]{3}>/, "")
     // Remove tag content or attributes (e.g. <b dfgfdg> => <b>)
     .replace(/<([u,i,b,c])(\..*?)?(?: .*?)?>(.*?)<\/\1>/g,
       "<$1$2>$3</$1$2>");
 
-  const parser = new DOMParser();
-  const parsedWebVTT = parser.parseFromString(filtered, "text/html");
+  const parsedWebVTT = new DOMParser().parseFromString(filteredText, "text/html");
   const nodes = parsedWebVTT.body.childNodes;
 
-  /**
-   * Apply styles to specifig tag in children nodes.
-   * (e.g. If class "b" has style, then : <b style="content">
-   * )
-   * Change class tags into span with associated style, or text*
-   * First it was: <c.class>...</c>. Then <class></class>.
-   * Finally <span style="content"></span> or text.
-   * @param {Array.<Node>} childNodes
-   * @returns {Array.<Node>}
-   */
-  function parseNode(nodeToParse : NodeList) : Array<HTMLElement|Text> {
-    const parsedNodeArray : Array<HTMLElement|Text> = [];
-    for (let i = 0; i < nodeToParse.length; i++) {
-      parsedNodeArray[i] = createStyleElement(nodeToParse[i]);
-    }
-
-    /**
-     * Construct an HTMLElement/TextNode representing the given node and apply
-     * the right styling on it.
-     * @param {Node} baseNode
-     * @returns {Node}
-     */
-    function createStyleElement(baseNode : Node) : HTMLElement|Text {
-      const mainTag = baseNode.nodeName.toLowerCase().split(".")[0];
-      let nodeWithStyle;
-      if (arrayIncludes(webVTTTags, mainTag)) { // If element accepted
-        if (mainTag === "#text") {
-          nodeWithStyle = document.createTextNode((baseNode as Text).wholeText);
-        } else {
-          const nodeClasses = baseNode.nodeName.toLowerCase().split(".");
-          const classIndexes : number[] = [];
-          nodeClasses.forEach(nodeClass => {
-            if (styleClasses.indexOf(nodeClass) !== -1) {
-              classIndexes.push(styleClasses.indexOf(nodeClass));
-            }
-          });
-          if (classIndexes.length !== 0) { // If style must be applied
-            const attr = document.createAttribute("style");
-            classIndexes.forEach(index => {
-              if (styleElements[index]) {
-                attr.value += styleElements[index].styleContent;
-              }
-            });
-            const nameClass = arrayIncludes(HTMLTags, mainTag) ? mainTag : "span";
-            nodeWithStyle = document.createElement(nameClass);
-            nodeWithStyle.setAttributeNode(attr);
-          } else { // If style mustn't be applied. Rebuild element with tag name
-            const elementTag = !arrayIncludes(HTMLTags, mainTag) ? "span" : mainTag;
-            nodeWithStyle = document.createElement(elementTag);
-          }
-          for (let j = 0; j < baseNode.childNodes.length; j++) {
-            nodeWithStyle.appendChild(
-              createStyleElement(baseNode.childNodes[j]));
-          }
-        }
-      } else {
-        nodeWithStyle = document.createElement("span");
-        for (let j = 0; j < baseNode.childNodes.length; j++) {
-          nodeWithStyle.appendChild(
-            createStyleElement(baseNode.childNodes[j]));
-        }
-      }
-
-      return nodeWithStyle;
-    }
-
-    return parsedNodeArray;
+  const parsedNodeArray : Array<HTMLElement|Text> = [];
+  for (let i = 0; i < nodes.length; i++) {
+    parsedNodeArray[i] = createStyledElement(nodes[i], styleElements, styleClasses);
   }
 
-  return parseNode(nodes);
+  return parsedNodeArray;
 }
