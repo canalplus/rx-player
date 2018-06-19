2fe4bc41aff366b21650fa6f107a819d45ce4129
peaBerberian <pea.berberian@gmail.com>
2018-06-19 18:50:32 +0200

    source-buffers: move TimedDataBufferManager out of html text tracks

--- src/core/source_buffers/text/html/html_text_source_buffer.ts
+++ src/core/source_buffers/text/html/html_text_source_buffer.ts
@@ -32,7 +32,7 @@ import { events } from "../../../../compat";
 import config from "../../../../config";
 import log from "../../../../log";
 import AbstractSourceBuffer from "../../abstract_source_buffer";
-import TimedDataBufferManager from "./buffer_manager";
+import TimedDataBufferManager from "../../buffer_manager";
 import parseTextTrackToElements from "./parsers";
 
 const { onEnded$,
