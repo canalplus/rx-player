e166cfb531f2c57df6cd335ff668a150f1c3e179
grenault73 <grenault73@gmail.com>
2018-07-05 12:04:30 +0200

    abr: fix getting current request when bitrate collapse

--- src/core/abr/representation_chooser.ts
+++ src/core/abr/representation_chooser.ts
@@ -146,7 +146,7 @@ function getConcernedRequest(
   const currentRequestIds = Object.keys(requests);
   const len = currentRequestIds.length;
 
-  for (let i = 0; i < len - 1; i++) {
+  for (let i = 0; i < len; i++) {
     const request = requests[currentRequestIds[i]];
 
     const {
