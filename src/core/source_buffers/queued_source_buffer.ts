d6bf40b7677b3f301cf9c9f5121d41e6f29aeaa7
peaBerberian <pea.berberian@gmail.com>
2018-08-14 16:29:30 +0200

    QueuedSourceBuffer: fix JSDoc

--- src/core/source_buffers/queued_source_buffer.ts
+++ src/core/source_buffers/queued_source_buffer.ts
@@ -199,9 +199,8 @@ export default class QueuedSourceBuffer<T> {
 
   /**
    * Remove data from the attached SourceBuffer, in a FIFO queue.
-   * @param {Object} range - Range of positions. With two properties:
-   *   - start {Number} - start position, in seconds
-   *   - end {Number} - end position, in seconds
+   * @param {number} start - start position, in seconds
+   * @param {number} end - end position, in seconds
    * @returns {Observable}
    */
   public removeBuffer(start : number, end : number) : Observable<void> {
