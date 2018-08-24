caac808dd20dd70979c30b5abe1122657d78250e
peaBerberian <pea.berberian@gmail.com>
2018-08-24 14:59:16 +0200

    update version

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -166,7 +166,7 @@ class Player extends EventEmitter<PLAYER_EVENT_STRINGS, any> {
    * Current version of the RxPlayer.
    * @type {string}
    */
-  public static version = /*PLAYER_VERSION*/"3.5.2";
+  public static version = /*PLAYER_VERSION*/"3.6.0";
 
   /**
    * Current version of the RxPlayer.
@@ -514,7 +514,7 @@ class Player extends EventEmitter<PLAYER_EVENT_STRINGS, any> {
     // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1194624
     videoElement.preload = "auto";
 
-    this.version = /*PLAYER_VERSION*/"3.5.2";
+    this.version = /*PLAYER_VERSION*/"3.6.0";
     this.log = log;
     this.state = "STOPPED";
     this.videoElement = videoElement;
