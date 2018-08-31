canalplus/rx-player

commit 64eafd4b2e8b473c2f8cb1c189a16abf8d2454df

stream: add warning when autoplay is blocked by the user-agent

src/core/stream/directfile.ts
@@ -22,18 +22,22 @@
 import {
   merge as observableMerge,
   Observable,
+  of as observableOf,
   Subject,
 } from "rxjs";
 import {
   ignoreElements,
   map,
-  mapTo,
+  mergeMap,
 } from "rxjs/operators";
 import {
   clearElementSrc,
   setElementSrc$,
 } from "../../compat";
-import { ICustomError } from "../../errors";
+import {
+  ICustomError,
+  MediaError,
+} from "../../errors";
 import log from "../../log";
 import { IKeySystemOption } from "../eme/types";
 import { IStreamClockTick } from "./clock";
@@ -45,7 +49,7 @@ import StallingManager from "./stalling_manager";
 import EVENTS, {
   IStreamEvent,
 } from "./stream_events";
-import handleInitialVideoEvents from "./video_events";
+import seekAndLoadOnMediaEvent from "./video_events";
 
 /**
  * @param {HTMLMediaElement} mediaElement
@@ -130,9 +134,9 @@ export default function StreamDirectFile({
   log.debug("initial time calculated:", initialTime);
 
   const {
-    initialSeek$,
-    loadAndPlay$,
-  } = handleInitialVideoEvents(mediaElement, initialTime, autoPlay);
+    seek$,
+    load$,
+  } = seekAndLoadOnMediaEvent(mediaElement, initialTime, autoPlay);
 
   /**
    * Create EME Manager, an observable which will manage every EME-related
@@ -166,15 +170,19 @@ export default function StreamDirectFile({
   const stallingManager$ = StallingManager(mediaElement, clock$)
     .pipe(map(EVENTS.stalled));
 
-  const loadedEvent$ = loadAndPlay$
-    .pipe(mapTo(EVENTS.loaded()));
+  const loadedEvent$ = load$
+    .pipe(mergeMap((evt) => {
+      if (evt === "autoplay-blocked") {
+        const error = new MediaError("AUTOPLAY_NOT_ALLOWED", null, false);
+        return observableOf(EVENTS.warning(error), EVENTS.loaded());
+      }
+      return observableOf(EVENTS.loaded);
+    }));
 
   const linkURL$ = setElementSrc$(mediaElement, url)
     .pipe(ignoreElements());
 
-  const mutedInitialSeek$ = initialSeek$
-    .pipe(ignoreElements());
-
+  const mutedInitialSeek$ = seek$.pipe(ignoreElements());
   const directFile$ : Observable<IStreamEvent> = observableMerge(
     loadedEvent$,
     mutedInitialSeek$,

src/core/stream/index.ts
@@ -26,7 +26,6 @@ import {
   finalize,
   ignoreElements,
   map,
-  mapTo,
   mergeMap,
   take,
   takeUntil,
@@ -83,7 +82,7 @@ import StallingManager from "./stalling_manager";
 import EVENTS, {
   IStreamEvent,
 } from "./stream_events";
-import handleInitialVideoEvents from "./video_events";
+import seekAndLoadOnMediaEvent from "./video_events";
 
 function getManifestPipelineOptions(
   networkConfig: {
@@ -254,11 +253,11 @@ export default function Stream({
     }
 
     const {
-      initialSeek$,
-      loadAndPlay$,
-    } = handleInitialVideoEvents(videoElement, initialTime, autoPlay);
+      seek$,
+      load$,
+    } = seekAndLoadOnMediaEvent(videoElement, initialTime, autoPlay);
 
-    const bufferClock$ = createBufferClock(manifest, clock$, initialSeek$, initialTime);
+    const bufferClock$ = createBufferClock(manifest, clock$, seek$, initialTime);
 
     /**
      * Subject through which network metrics will be sent by the segment
@@ -395,7 +394,15 @@ export default function Stream({
 
     // Single lifecycle events
     const manifestReadyEvent$ = observableOf(EVENTS.manifestReady(abrManager, manifest));
-    const loadedEvent$ = loadAndPlay$.pipe(mapTo(EVENTS.loaded()));
+
+    const loadedEvent$ = load$
+      .pipe(mergeMap((evt) => {
+        if (evt === "autoplay-blocked") {
+          const error = new MediaError("AUTOPLAY_NOT_ALLOWED", null, false);
+          return observableOf(EVENTS.warning(error), EVENTS.loaded());
+        }
+        return observableOf(EVENTS.loaded);
+      }));
 
     return observableMerge(
       manifestReadyEvent$,

src/core/stream/video_events.ts
@@ -15,42 +15,44 @@
  */
 
 import {
-  combineLatest as observableCombineLatest,
   Observable,
   of as observableOf,
 } from "rxjs";
 import {
-  mergeMapTo,
+  catchError,
+  mapTo,
+  mergeMap,
   shareReplay,
   tap,
 } from "rxjs/operators";
 import {
   canPlay,
   hasLoadedMetadata,
-  playUnlessAutoPlayPolicy$,
+  play$,
 } from "../../compat";
 import log from "../../log";
 
 /**
  * Set the initial time given as soon as possible on the video element.
  * Emit when done.
- * @param {HMTLMediaElement} videoElement
- * @param {number|Function} startTime
+ * @param {HMTLMediaElement} mediaElement
+ * @param {number|Function} startTime - Initial starting position. As seconds
+ * or as a function returning seconds.
  * @returns {Observable}
  */
 function doInitialSeek(
-  videoElement : HTMLMediaElement,
+  mediaElement : HTMLMediaElement,
   startTime : number|(() => number)
 ) : Observable<void> {
-  return hasLoadedMetadata(videoElement)
+  return hasLoadedMetadata(mediaElement)
     .pipe(
       tap(() => {
         log.info("set initial time", startTime);
 
         // reset playbackRate to 1 in case we were at 0 (from a stalled
         // retry for instance)
-        videoElement.playbackRate = 1;
-        videoElement.currentTime = typeof startTime === "function" ?
+        mediaElement.playbackRate = 1;
+        mediaElement.currentTime = typeof startTime === "function" ?
           startTime() : startTime;
       }),
       shareReplay() // we don't want to repeat the side-effect on each
@@ -59,37 +61,47 @@ function doInitialSeek(
 }
 
 /**
- * @param {HTMLMediaElement} videoElement
- * @param {number|Function} startTime
- * @param {boolean} autoPlay
+ * @param {HTMLMediaElement} mediaElement
+ * @param {number|Function} startTime - Initial starting position. As seconds
+ * or as a function returning seconds.
+ * @param {boolean} autoPlay - Whether the player should auto-play
  * @returns {object}
  */
-export default function handleVideoEvents(
-  videoElement : HTMLMediaElement,
+export default function seekAndLoadOnMediaEvent(
+  mediaElement : HTMLMediaElement,
   startTime : number|(() => number),
   mustAutoPlay : boolean
 ) : {
-  initialSeek$ : Observable<void>;
-  loadAndPlay$ : Observable<void>;
+  seek$ : Observable<void>;
+  load$ : Observable<"autoplay-blocked"|"autoplay"|"loaded">;
 } {
-  const initialSeek$ = doInitialSeek(videoElement, startTime);
-  const handledCanPlay$ = canPlay(videoElement).pipe(
-    tap(() => log.info("canplay event"))
+  const seek$ = doInitialSeek(mediaElement, startTime);
+
+  const load$ = canPlay(mediaElement).pipe(
+
+    tap(() => log.info("canplay event")),
+
+    mergeMap(() => {
+      if (mustAutoPlay) {
+        return play$(mediaElement).pipe(
+          mapTo("autoplay" as "autoplay"),
+          catchError((error) => {
+            if (error.name === "NotAllowedError") {
+              // auto-play was probably prevented.
+              log.warn("Media element can't play." +
+                " It may be due to browser auto-play policies.");
+              return observableOf("autoplay-blocked" as "autoplay-blocked");
+            } else {
+              throw error;
+            }
+          })
+        );
+      }
+      return observableOf("loaded" as "loaded");
+    }),
+
+    shareReplay()
   );
 
-  const loadAndPlay$ = observableCombineLatest(
-    initialSeek$,
-    handledCanPlay$
-  ).pipe(
-    mergeMapTo(mustAutoPlay ?
-      playUnlessAutoPlayPolicy$(videoElement) :
-      observableOf(undefined)
-    ),
-    shareReplay() // avoid doing "play" each time someone subscribes
-  );
-
-  return {
-    initialSeek$,
-    loadAndPlay$,
-  };
+  return { seek$, load$ };
 }

src/errors/constants.ts
@@ -57,6 +57,8 @@ const ErrorCodes = {
   BUFFER_FULL_ERROR: "BUFFER_FULL_ERROR",
   BUFFER_TYPE_UNKNOWN: "BUFFER_TYPE_UNKNOWN",
 
+  MEDIA_ERR_BLOCKED_AUTOPLAY: "MEDIA_ERR_BLOCKED_AUTOPLAY",
+
   MEDIA_ERR_ABORTED: "MEDIA_ERR_ABORTED",
   MEDIA_ERR_NETWORK: "MEDIA_ERR_NETWORK",
   MEDIA_ERR_DECODE: "MEDIA_ERR_DECODE",

commit 071753e6cb6ac4aa5e0f69f630b66312500b4d08

direct: fix loadedEvent
Fixes a bug introduced in 64eafd4b, that cause the loadedEvent to emit
the loaded function instead of the loaded event object.

src/core/stream/directfile.ts
@@ -169,7 +169,7 @@ export default function StreamDirectFile({
         const error = new MediaError("MEDIA_ERR_BLOCKED_AUTOPLAY", null, false);
         return observableOf(EVENTS.warning(error), EVENTS.loaded());
       }
-      return observableOf(EVENTS.loaded);
+      return observableOf(EVENTS.loaded());
     }));
 
   // Start everything! (Just put the URL in the element's src).