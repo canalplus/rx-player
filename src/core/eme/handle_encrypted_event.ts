4099123d717e5e00cd1a992f63b32f92f3faf421
peaBerberian <pea.berberian@gmail.com>
2018-08-31 15:47:46 +0200

    eme: fix closeAllSessions logic in the OpenSessionsStore

--- src/core/eme/handle_encrypted_event.ts
+++ src/core/eme/handle_encrypted_event.ts
@@ -103,14 +103,16 @@ export default function handleEncryptedEvent(
     }
 
     return (previousLoadedSession ?
-      sessionsStore.closeSession(previousLoadedSession) :
+      sessionsStore.deleteAndCloseSession(previousLoadedSession) :
       observableOf(null)
     ).pipe(mergeMap(() => {
       const cleaningOldSessions$ : Array<Observable<null>> = [];
       const entries = sessionsStore.getAll().slice();
       if (MAX_SESSIONS > 0 && MAX_SESSIONS <= entries.length) {
         for (let i = 0; i < (MAX_SESSIONS - entries.length + 1); i++) {
-          cleaningOldSessions$.push(sessionsStore.closeSession(entries[i].session));
+          cleaningOldSessions$.push(
+            sessionsStore.deleteAndCloseSession(entries[i].session)
+          );
         }
       }
 
