28b5b8759336eecef123286706f978782a5ce111
grenault73 <grenault73@gmail.com>
2018-09-12 10:50:54 +0200

    demo: add github buttons + player version

--- demo/full/scripts/controllers/Main.jsx
+++ demo/full/scripts/controllers/Main.jsx
@@ -4,7 +4,13 @@ import Player from "./Player.jsx";
 
 export default () => (
   <div>
-    <Title title="Rx Player Demo" />
+    <div class="nav-header">
+      <Title title="Rx-Player v3.6.2" />
+      <div class="github-buttons">
+        <a class="github-button" href="https://github.com/canalplus/rx-player" data-size="large" data-icon="octicon-star" data-show-count="true" aria-label="Star canalplus/rx-player on GitHub">Star</a>
+        <a class="github-button" href="https://github.com/canalplus/rx-player/fork" data-size="large" aria-label="Fork canalplus/rx-player on GitHub">Fork</a>
+      </div>
+    </div>
     <Player />
   </div>
 );
