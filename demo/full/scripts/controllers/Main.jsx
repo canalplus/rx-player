4fa91f6c20ef0f828aedf7e9c171283b8665d4ff
peaBerberian <pea.berberian@gmail.com>
2018-09-13 16:43:01 +0200

    demo: add static version

--- demo/full/scripts/controllers/Main.jsx
+++ demo/full/scripts/controllers/Main.jsx
@@ -5,7 +5,7 @@ import Player from "./Player.jsx";
 export default () => (
   <div>
     <div class="nav-header">
-      <Title title="Rx-Player v3.6.2" />
+      <Title title={"Rx-Player v" + window.RxPlayer.version} />
       <div class="github-buttons">
         <a class="github-button" href="https://github.com/canalplus/rx-player" data-size="large" data-icon="octicon-star" data-show-count="true" aria-label="Star canalplus/rx-player on GitHub">Star</a>
         <a class="github-button" href="https://github.com/canalplus/rx-player/fork" data-size="large" aria-label="Fork canalplus/rx-player on GitHub">Fork</a>
