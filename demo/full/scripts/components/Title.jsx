0a676102c5297f1d1ddf83e2d4a1058027a7ed77
peaBerberian <pea.berberian@gmail.com>
2018-09-14 16:38:19 +0200

    demo: add logo to header

--- demo/full/scripts/components/Title.jsx
+++ demo/full/scripts/components/Title.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default ({ title }) =>
-  <section className="title-wrapper">
-    <h1 className="title">
-      <span className="light">{title}</span>
-    </h1>
-  </section>;
+  <a href="https://github.com/canalplus/rx-player">
+    <section className="title-wrapper">
+      <h1 className="title">
+        <span className="light">{title}</span>
+      </h1>
+    </section>
+  </a>;
--- demo/full/scripts/controllers/Main.jsx
+++ demo/full/scripts/controllers/Main.jsx
@@ -6,9 +6,10 @@ export default () => (
   <div>
     <div class="nav-header">
       <Title title={"Rx-Player v" + window.RxPlayer.version} />
-      <div class="github-buttons">
-        <a class="github-button" href="https://github.com/canalplus/rx-player" data-size="large" data-icon="octicon-star" data-show-count="true" aria-label="Star canalplus/rx-player on GitHub">Star</a>
-        <a class="github-button" href="https://github.com/canalplus/rx-player/fork" data-size="large" aria-label="Fork canalplus/rx-player on GitHub">Fork</a>
+      <div className="header-links-buttons">
+        <a href="https://www.mycanal.fr"><img className="title-logo" src="../../assets/canalp.svg" /></a>
+        <a className="github-button" href="https://github.com/canalplus/rx-player" data-size="large" data-icon="octicon-star" data-show-count="true" aria-label="Star canalplus/rx-player on GitHub">Star</a>
+        <a className="github-button" href="https://github.com/canalplus/rx-player/fork" data-size="large" aria-label="Fork canalplus/rx-player on GitHub">Fork</a>
       </div>
     </div>
     <Player />
