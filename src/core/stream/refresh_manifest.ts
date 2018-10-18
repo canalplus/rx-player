0b664713d7c0f99240d3c424227eaedfb002ae08
grenault73 <grenault73@gmail.com>
2018-10-18 16:48:25 +0200

    stream: refreshManifest lint

--- src/core/stream/refresh_manifest.ts
+++ src/core/stream/refresh_manifest.ts
@@ -1,9 +1,32 @@
-import { Observable, EMPTY } from "rxjs";
-import { tap, share, mapTo } from "rxjs/operators";
-import Manifest from "../../manifest";
+/**
+ * Copyright 2015 CANAL+ Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  EMPTY,
+  Observable,
+} from "rxjs";
+import {
+  mapTo,
+  share,
+  tap,
+} from "rxjs/operators";
 import log from "../../log";
-import { IManifestUpdateEvent } from "./types";
+import Manifest from "../../manifest";
 import EVENTS from "./events_generators";
+import { IManifestUpdateEvent } from "./types";
 
 /**
  * Re-fetch the manifest and merge it with the previous version.
