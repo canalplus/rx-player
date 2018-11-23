bac58b26f2efe7a42c9c3bda508b9cb5ada93843
grenault73 <grenault73@gmail.com>
2018-11-23 10:58:52 +0100

    compat: removed samsung regex

--- src/compat/constants.ts
+++ src/compat/constants.ts
@@ -130,9 +130,6 @@ const isIEOrEdge : boolean =
 const isFirefox : boolean =
   navigator.userAgent.toLowerCase().indexOf("firefox") !== -1;
 
-const isSamsungInternet : boolean =
-  navigator.userAgent.toLowerCase().indexOf("SamsungBro") !== -1;
-
 const READY_STATES = {
   HAVE_NOTHING: 0,
   HAVE_METADATA: 1,
