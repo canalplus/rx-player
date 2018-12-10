9ac2a79801d28c5a1b619b13a249a6a7fa504f5e
grenault73 <grenault73@gmail.com>
2018-12-10 20:08:06 +0100

    tests: unit tests for flattenOverlappingPeriods

--- src/parsers/manifest/dash/__tests__/index.test.ts
+++ src/parsers/manifest/dash/__tests__/index.test.ts
@@ -17,6 +17,7 @@
 import { expect } from "chai";
 // import sinon from "sinon";
 
+import flattenOverlappingPeriods from "../flatten_overlapping_periods";
 import parseFromDocument from "../index";
 
 // const mpd = require("raw!test/fixtures/dash-seg-list.mpd");
@@ -34,6 +35,91 @@ describe("dash parser", function() {
       expect(function() {
         parseFromDocument(doc, "");
       }).to.throw("document root should be MPD");
+    });
+  });
+
+  describe("flattenOverlappingPeriods", function() {
+    // [ Period 1 ][ Period 2 ]       ------>  [ Period 1 ][ Period 3 ]
+    //             [ Period 3 ]
+    it("flatten periods - case 1", function() {
+      const periods = [
+        { id: "1", start: 0, duration: 60, adaptations: {} },
+        { id: "2", start: 60, duration: 60, adaptations: {} },
+        { id: "3", start: 60, duration: 60, adaptations: {} },
+      ];
+
+      const flattenPeriods = flattenOverlappingPeriods(periods);
+      expect(flattenPeriods.length).to.be.equal(2);
+      expect(flattenPeriods[0].start).to.be.equal(0);
+      expect(flattenPeriods[0].duration).to.be.equal(60);
+      expect(flattenPeriods[0].id).to.be.equal("1");
+      expect(flattenPeriods[1].start).to.be.equal(60);
+      expect(flattenPeriods[1].duration).to.be.equal(60);
+      expect(flattenPeriods[1].id).to.be.equal("3");
+    });
+
+    // [ Period 1 ][ Period 2 ]       ------>  [ Period 1 ][  2  ][ Period 3 ]
+    //                  [ Period 3 ]
+    it("flatten periods - case 2", function() {
+      const periods = [
+        { id: "1", start: 0, duration: 60, adaptations: {} },
+        { id: "2", start: 60, duration: 60, adaptations: {} },
+        { id: "3", start: 90, duration: 60, adaptations: {} },
+      ];
+
+      const flattenPeriods = flattenOverlappingPeriods(periods);
+      expect(flattenPeriods.length).to.be.equal(3);
+      expect(flattenPeriods[0].start).to.be.equal(0);
+      expect(flattenPeriods[0].duration).to.be.equal(60);
+      expect(flattenPeriods[0].id).to.be.equal("1");
+      expect(flattenPeriods[1].start).to.be.equal(60);
+      expect(flattenPeriods[1].duration).to.be.equal(30);
+      expect(flattenPeriods[1].id).to.be.equal("2");
+      expect(flattenPeriods[2].start).to.be.equal(90);
+      expect(flattenPeriods[2].duration).to.be.equal(60);
+      expect(flattenPeriods[2].id).to.be.equal("3");
+    });
+
+    // [ Period 1 ][ Period 2 ]       ------>  [  1  ][      Period 3     ]
+    //        [      Period 3     ]
+    it("flatten periods - case 3", function() {
+      const periods = [
+        { id: "1", start: 0, duration: 60, adaptations: {} },
+        { id: "2", start: 60, duration: 60, adaptations: {} },
+        { id: "3", start: 50, duration: 120, adaptations: {} },
+      ];
+
+      const flattenPeriods = flattenOverlappingPeriods(periods);
+      expect(flattenPeriods.length).to.be.equal(2);
+      expect(flattenPeriods[0].start).to.be.equal(0);
+      expect(flattenPeriods[0].duration).to.be.equal(50);
+      expect(flattenPeriods[0].id).to.be.equal("1");
+      expect(flattenPeriods[1].start).to.be.equal(50);
+      expect(flattenPeriods[1].duration).to.be.equal(120);
+      expect(flattenPeriods[1].id).to.be.equal("3");
+    });
+
+    // [ Period 1 ][ Period 2 ]       ------>  [  1  ][  100   ]
+    //             [ Period 3 ]
+    //                  ...
+    //             [   100    ]
+    it("flatten periods - case 4", function() {
+      const periods = [
+        { id: "1", start: 0, duration: 60, adaptations: {} },
+      ];
+
+      for (let i = 1; i <= 100; i++) {
+        periods.push({ id: i.toString(), start: 60, duration: 60, adaptations: {} });
+      }
+
+      const flattenPeriods = flattenOverlappingPeriods(periods);
+      expect(flattenPeriods.length).to.be.equal(2);
+      expect(flattenPeriods[0].start).to.be.equal(0);
+      expect(flattenPeriods[0].duration).to.be.equal(60);
+      expect(flattenPeriods[0].id).to.be.equal("1");
+      expect(flattenPeriods[1].start).to.be.equal(60);
+      expect(flattenPeriods[1].duration).to.be.equal(60);
+      expect(flattenPeriods[1].id).to.be.equal("100");
     });
   });
 });
