e23a646fa08d4199804725951b8d6117796dcf08
peaBerberian <pea.berberian@gmail.com>
2018-12-18 16:13:03 +0100

    utils/tests: finish tests for concatMapLatest

--- src/utils/__tests__/concat_map_latest.test.ts
+++ src/utils/__tests__/concat_map_latest.test.ts
@@ -17,26 +17,64 @@
 import { expect } from "chai";
 
 import {
-  from as observableFrom,
+  interval,
   Observable,
   of as observableOf,
   timer,
 } from "rxjs";
 import {
   concatMap,
-  delay,
   mapTo,
+  take,
 } from "rxjs/operators";
 import concatMapLatest from "../concat_map_latest";
 
 describe("utils - concatMapLatest", () => {
-  it("should output all inner values", (done) => {
+  it("should act as a mergeMap for a single value", (done) => {
+    const counter$ : Observable<number> = observableOf(0);
+    let itemReceived = false;
+    return counter$.pipe(
+      /* tslint:disable no-unnecessary-callback-wrapper */
+      concatMapLatest((i: number) => observableOf(i))
+      /* tslint:enable no-unnecessary-callback-wrapper */
+    ).subscribe((res: number) => {
+      expect(res).to.equal(0);
+      itemReceived = true;
+    }, undefined, () => {
+      expect(itemReceived).to.equal(true);
+      done();
+    });
+  });
+
+  /* tslint:disable:max-line-length */
+  it("should consider all values if precedent inner Observable finished synchronously", (done) => {
+  /* tslint:enable:max-line-length */
     const innerValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    const counter$: Observable<number> = observableFrom(innerValues).pipe(
+    const counter$ : Observable<number> = observableOf(...innerValues);
+    return counter$.pipe(
+      /* tslint:disable no-unnecessary-callback-wrapper */
+      concatMapLatest((i: number) => observableOf(i))
+      /* tslint:enable no-unnecessary-callback-wrapper */
+    ).subscribe((res: number) => {
+      const expectedResult = innerValues.shift();
+      expect(res).to.equal(expectedResult);
+    }, undefined, () => {
+      if (innerValues.length !== 0) {
+        throw new Error("Not all values were received.");
+      }
+      done();
+    });
+  });
+
+  /* tslint:disable:max-line-length */
+  it("should consider all values if precedent inner Observable had time to finish", (done) => {
+  /* tslint:enable:max-line-length */
+    const innerValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const counter$ : Observable<number> = observableOf(...innerValues).pipe(
       concatMap((v) => timer(5).pipe(mapTo(v)))
     );
-
     return counter$.pipe(
       /* tslint:disable no-unnecessary-callback-wrapper */
       concatMapLatest((i: number) => observableOf(i))
@@ -44,32 +82,30 @@ describe("utils - concatMapLatest", () => {
     ).subscribe((res: number) => {
       const expectedResult = innerValues.shift();
       expect(res).to.be.equal(expectedResult);
-      if (innerValues.length === 0) {
-        done();
-        return;
+    }, undefined, () => {
+      if (innerValues.length !== 0) {
+        throw new Error("Not all values were received.");
       }
+      done();
     });
   });
 
-  it("should skip all inner values but the last", (done) => {
-    const innerValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const expectedResults = [0, 9];
-
-    const counter$: Observable<number> = observableFrom(innerValues);
+  /* tslint:disable:max-line-length */
+  it("should skip all inner values but the last when the inner Observable completes", (done) => {
+  /* tslint:enable:max-line-length */
+    const expectedResults = [0, 2, 4, 6, 8, 9];
+    const counter$ : Observable<number> = interval(100).pipe(take(10));
 
     return counter$.pipe(
-      concatMapLatest((i: number) => {
-        return observableOf(i).pipe(
-          delay(30)
-        );
-      })
+      concatMapLatest((i: number) => timer(210).pipe(mapTo(i)))
     ).subscribe((result: number) => {
       const expectedResult = expectedResults.shift();
-      expect(result).to.be.equal(expectedResult);
-      if (expectedResults.length === 0) {
-        done();
-        return;
+      expect(result).to.equal(expectedResult);
+    }, undefined, () => {
+      if (expectedResults.length !== 0) {
+        throw new Error("Not all values were received.");
       }
+      done();
     });
   });
 });
