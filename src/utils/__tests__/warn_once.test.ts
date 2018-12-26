9dfebfc6eec507f5495356a2ab23d3c7dc26e4d3
peaBerberian <pea.berberian@gmail.com>
2018-12-26 13:44:21 +0100

    utils/tests: add unit tests to most utils

---
+++ src/utils/__tests__/warn_once.test.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2015 CANAL+ Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { expect } from "chai";
+import * as sinon from "sinon";
+import warnOnce from "../warn_once";
+
+describe("utils - warnOnce", () => {
+  it("should only call console.warn once for a given message", () => {
+    const warnSpy = sinon.spy(console, "warn");
+    warnOnce("toto titi");
+
+    expect(warnSpy.calledWith("toto titi")).to.equal(true);
+    expect(warnSpy.calledOnce).to.equal(true);
+
+    warnOnce("toto titi");
+
+    expect(warnSpy.calledOnce).to.equal(true);
+
+    warnOnce("toto titi");
+    warnOnce("toto tito");
+
+    expect(warnSpy.calledWith("toto tito")).to.equal(true);
+    expect(warnSpy.calledTwice).to.equal(true);
+  });
+});
