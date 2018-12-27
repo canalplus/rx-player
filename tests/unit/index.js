e1eed3263131a776e086e2bdb1cfee9448129b6d
peaBerberian <pea.berberian@gmail.com>
2018-12-27 19:26:17 +0100

    ci: add babel polyfill for appveyor

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,11 +1,13 @@
+require("@babel/polyfill"); // required for some old targets (e.g. IE11)
+
 /**
  * Require all files returned by a require.context call
  * @param {Function} requireContext - Return of a require.context.
  * @return {Array} - Array containing every modules.
  */
-const requireAll = function(requireContext) {
+function requireAll(requireContext) {
   return requireContext.keys().forEach(requireContext);
-};
+}
 
 // require all modules ending in ".test.js" from the
 // core directory and all subdirectories
