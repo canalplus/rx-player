fdbfb2b8bdbb6ef0130cd752a07ec440ee7b75e9
peaBerberian <pea.berberian@gmail.com>
2019-01-05 01:03:24 +0100

    tests: perform unit tests with jest now

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   "src/**/*.ts",
+  //   "!**/__tests__/**",
+  // ],
+  globals: {
+    __DEV__: true,
+    "ts-jest": {
+      tsConfig: {
+        allowJs: true,
+        target: "es2017",
+        lib: ["es2017", "dom"],
+        forceConsistentCasingInFileNames: true,
+        skipLibCheck: false,
+        noImplicitAny: true,
+        strict: true,
+        strictNullChecks: true,
+        strictPropertyInitialization: true,
+        noUnusedParameters: true,
+        noUnusedLocals: true,
+        types: ["rxjs", "node", "jest"],
+        module: "commonjs",
+        moduleResolution: "node",
+        typeRoots: [
+          "./src/typings",
+          "./node_modules/@types",
+        ],
+      },
+    },
+  },
+};
