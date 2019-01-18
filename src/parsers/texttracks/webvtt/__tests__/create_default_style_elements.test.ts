20c9bd70c6cd535b0f2dedfd5d80d92d5bcdce20
grenault73 <grenault73@gmail.com>
2019-01-18 16:17:13 +0100

    parsers: test default webvtt style elements creator

---
+++ src/parsers/texttracks/webvtt/__tests__/create_default_style_elements.test.ts
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2015 CANAL+ Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import createDefaultStyleElements from "../html/create_default_style_elements";
+
+describe("parsers - webvtt - createDefaultStyleElements", () => {
+  it("should return expected default style elements", () => {
+    expect(createDefaultStyleElements()).toEqual(
+      [
+        {
+          className: "white",
+          isGlobalStyle: false,
+          styleContent: "color: #ffffff",
+        },
+        {
+          className: "bg_white",
+          isGlobalStyle: false,
+          styleContent: "background-color: #ffffff",
+        },
+        {
+          className: "lime",
+          isGlobalStyle: false,
+          styleContent: "color: #00ff00",
+        }, {
+          className: "bg_lime",
+          isGlobalStyle: false,
+          styleContent: "background-color: #00ff00",
+        },
+        {
+          className: "cyan",
+          isGlobalStyle: false,
+          styleContent: "color: #00ffff",
+        },
+        {
+          className: "bg_cyan",
+          isGlobalStyle: false,
+          styleContent: "background-color: #00ffff",
+        },
+        {
+          className: "red",
+          isGlobalStyle: false,
+          styleContent: "color: #ff0000",
+        },
+        {
+          className: "bg_red",
+          isGlobalStyle: false,
+          styleContent: "background-color: #ff0000",
+        }, {
+          className: "yellow",
+          isGlobalStyle: false,
+          styleContent: "color: #ffff00",
+        },
+        {
+          className: "bg_yellow",
+          isGlobalStyle: false,
+          styleContent: "background-color: #ffff00",
+        },
+        {
+          className: "magenta",
+          isGlobalStyle: false,
+          styleContent: "color: #ff00ff",
+        },
+        {
+          className: "bg_magenta",
+          isGlobalStyle: false,
+          styleContent: "background-color: #ff00ff",
+        },
+        {
+          className: "blue",
+          isGlobalStyle: false,
+          styleContent: "color: #0000ff",
+        },
+        {
+          className: "bg_blue",
+          isGlobalStyle: false,
+          styleContent: "background-color: #0000ff",
+        },
+        {
+          className: "black",
+          isGlobalStyle: false,
+          styleContent: "color: #000000",
+        },
+        {
+          className: "bg_black",
+          isGlobalStyle: false,
+          styleContent: "background-color: #000000",
+        },
+      ]
+    );
+  });
+});
