70cad73a570763116602323c1872bfb6e782aece
grenault73 <grenault73@gmail.com>
2019-01-23 11:47:10 +0100

    parsers: fix default style element formatting

--- src/parsers/texttracks/webvtt/__tests__/create_default_style_elements.test.ts
+++ src/parsers/texttracks/webvtt/__tests__/create_default_style_elements.test.ts
@@ -20,22 +20,22 @@ describe("parsers - webvtt - createDefaultStyleElements", () => {
   it("should return expected default style elements", () => {
     expect(createDefaultStyleElements()).toEqual(
       {
-        white: "color: #ffffff",
-        bg_white: "background-color: #ffffff",
-        lime: "color: #00ff00",
-        bg_lime: "background-color: #00ff00",
-        cyan: "color: #00ffff",
-        bg_cyan: "background-color: #00ffff",
-        red: "color: #ff0000",
-        bg_red: "background-color: #ff0000",
-        yellow: "color: #ffff00",
-        bg_yellow: "background-color: #ffff00",
-        magenta: "color: #ff00ff",
-        bg_magenta: "background-color: #ff00ff",
-        blue: "color: #0000ff",
-        bg_blue: "background-color: #0000ff",
-        black: "color: #000000",
-        bg_black: "background-color: #000000",
+        white: "color: #ffffff;",
+        bg_white: "background-color: #ffffff;",
+        lime: "color: #00ff00;",
+        bg_lime: "background-color: #00ff00;",
+        cyan: "color: #00ffff;",
+        bg_cyan: "background-color: #00ffff;",
+        red: "color: #ff0000;",
+        bg_red: "background-color: #ff0000;",
+        yellow: "color: #ffff00;",
+        bg_yellow: "background-color: #ffff00;",
+        magenta: "color: #ff00ff;",
+        bg_magenta: "background-color: #ff00ff;",
+        blue: "color: #0000ff;",
+        bg_blue: "background-color: #0000ff;",
+        black: "color: #000000;",
+        bg_black: "background-color: #000000;",
       }
     );
   });
