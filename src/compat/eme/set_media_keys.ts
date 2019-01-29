cad5857d8711e5633b55c99548e69badb5ae1b45
grenault73 <grenault73@gmail.com>
2019-01-29 11:47:49 +0100

    compat: set media keys without waiting for IE/EDGE readyState to be >= 1

--- src/compat/eme/set_media_keys.ts
+++ src/compat/eme/set_media_keys.ts
@@ -18,10 +18,7 @@ import {
   defer as observableDefer,
   Observable
 } from "rxjs";
-import { mergeMap } from "rxjs/operators";
 import castToObservable from "../../utils/cast_to_observable";
-import { isIEOrEdge } from "../browser_detection";
-import { onLoadedMetadata$ } from "../event_listeners";
 import CustomMediaKeys, {
   ICustomMediaKeys,
 } from "./custom_media_keys";
@@ -72,17 +69,5 @@ export default function setMediaKeys$(
   elt : HTMLMediaElement,
   mediaKeys : MediaKeys|ICustomMediaKeys|null
 ) : Observable<unknown> {
-  return observableDefer(() => {
-    const _setMediaKeys$ = observableDefer(() =>
-      castToObservable(_setMediaKeys(elt, mediaKeys)));
-
-    // IE11/Edge requires that the media element readyState >= 1 before mediaKeys can be
-    // set, so wait for loadedmetadata
-    if (isIEOrEdge && elt.readyState < 1) {
-      return onLoadedMetadata$(elt).pipe(
-        mergeMap(() => _setMediaKeys$)
-      );
-    }
-    return _setMediaKeys$;
-  });
+  return observableDefer(() => castToObservable(_setMediaKeys(elt, mediaKeys)));
 }
