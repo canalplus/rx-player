f2c3973bdcd3673072d97e779c45919e08f437a2
peaBerberian <pea.berberian@gmail.com>
2019-02-15 17:52:17 +0100

    error: add reasons for every OtherError

--- src/errors/__tests__/other_error.test.ts
+++ src/errors/__tests__/other_error.test.ts
@@ -17,27 +17,14 @@
 import OtherError from "../other_error";
 
 describe("errors - OtherError", () => {
-  it("should format an OtherError when called with minimal arguments", () => {
-    const otherError = new OtherError("foo", null);
+  it("should format an OtherError", () => {
+    const otherError = new OtherError("foo", "tata", false);
     expect(otherError).toBeInstanceOf(Error);
     expect(otherError.name).toBe("OtherError");
     expect(otherError.type).toBe("OTHER_ERROR");
-    expect(otherError.reason).toBe(null);
     expect(otherError.code).toBe("");
     expect(otherError.fatal).toBe(false);
-    expect(otherError.message).toBe("OtherError ()");
-  });
-
-  it("should be able to give a reason", () => {
-    const reason = "test";
-    const otherError = new OtherError("foo", reason);
-    expect(otherError).toBeInstanceOf(Error);
-    expect(otherError.name).toBe("OtherError");
-    expect(otherError.type).toBe("OTHER_ERROR");
-    expect(otherError.reason).toBe(reason);
-    expect(otherError.code).toBe("");
-    expect(otherError.fatal).toBe(false);
-    expect(otherError.message).toBe("OtherError () test");
+    expect(otherError.message).toBe("OtherError () tata");
   });
 
   it("should be able to set it as fatal", () => {
@@ -46,32 +33,17 @@ describe("errors - OtherError", () => {
     expect(otherError).toBeInstanceOf(Error);
     expect(otherError.name).toBe("OtherError");
     expect(otherError.type).toBe("OTHER_ERROR");
-    expect(otherError.reason).toBe(reason);
     expect(otherError.code).toBe("");
     expect(otherError.fatal).toBe(true);
     expect(otherError.message).toBe("OtherError () test");
   });
 
   it("should filter in a valid error code", () => {
-    const otherError = new OtherError("MEDIA_ERR_NETWORK", null, true);
-    expect(otherError).toBeInstanceOf(Error);
-    expect(otherError.name).toBe("OtherError");
-    expect(otherError.type).toBe("OTHER_ERROR");
-    expect(otherError.reason).toBe(null);
-    expect(otherError.code).toBe("MEDIA_ERR_NETWORK");
-    expect(otherError.fatal).toBe(true);
-    expect(otherError.message).toBe("OtherError (MEDIA_ERR_NETWORK)");
-  });
-
-  /* tslint:disable max-line-length */
-  it("should set a complete error message if both a valid code and a reason is given", () => {
-  /* tslint:enable max-line-length */
     const reason = "test";
     const otherError = new OtherError("MEDIA_ERR_NETWORK", reason, true);
     expect(otherError).toBeInstanceOf(Error);
     expect(otherError.name).toBe("OtherError");
     expect(otherError.type).toBe("OTHER_ERROR");
-    expect(otherError.reason).toBe(reason);
     expect(otherError.code).toBe("MEDIA_ERR_NETWORK");
     expect(otherError.fatal).toBe(true);
     expect(otherError.message).toBe("OtherError (MEDIA_ERR_NETWORK) test");
