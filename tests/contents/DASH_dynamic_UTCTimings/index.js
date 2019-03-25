2e2b34f485d35239499ff2cfd4dafe51e2bcf489
peaBerberian <pea.berberian@gmail.com>
2019-03-25 16:49:52 +0100

    tests: add integration tests to ensure DASH UTCTimings is managed

---
+++ tests/contents/DASH_dynamic_UTCTimings/index.js
@@ -0,0 +1,11 @@
+import * as WithHTTP from "./with_http.js";
+import * as WithDirect from "./with_direct.js";
+import * as WithDirectAndHTTP from "./with_direct_and_http.js";
+import * as WithoutTimings from "./without_timings.js";
+
+export {
+  WithHTTP,
+  WithDirect,
+  WithDirectAndHTTP,
+  WithoutTimings,
+};
