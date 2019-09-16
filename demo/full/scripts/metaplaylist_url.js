e2a805bf53ed69b67fd6ef58962410c8e58e615e
peaBerberian <pea.berberian@gmail.com>
2019-09-16 12:51:20 +0200

    demo: remove last content in MetaPlaylist

--- demo/full/scripts/metaplaylist_url.js
+++ demo/full/scripts/metaplaylist_url.js
@@ -6,20 +6,14 @@ const MetaPlaylistDASHSmooth = {
     {
       "url": "https://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd",
       "startTime": 0,
-      "endTime": 733.95,
+      "endTime": 733.3,
       "transport": "dash",
     },
     {
       "url": "https://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest",
-      "startTime": 733.95,
-      "endTime": 1366.85,
+      "startTime": 733.3,
+      "endTime": 1368,
       "transport": "smooth",
-    },
-    {
-      "url": "https://demo.unified-streaming.com/video/ateam/ateam.ism/ateam.mpd",
-      "startTime": 1366.85,
-      "endTime": 1470,
-      "transport": "dash",
     },
   ],
 };
