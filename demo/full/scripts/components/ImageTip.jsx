8da5ae460e7a254038aa23472511b91eb480925a
peaBerberian <pea.berberian@gmail.com>
2019-11-06 19:04:04 +0100

    demo: set cleaner ImageTip implementation with hooks

--- demo/full/scripts/components/ImageTip.jsx
+++ demo/full/scripts/components/ImageTip.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 /**
  * React Component which Displays an Image tip centered and on top of the
@@ -12,86 +16,36 @@ import React from "react";
  *
  * @class ImageTip
  */
-class ImageTip extends React.Component {
-  constructor(...args) {
-    super(...args);
-
-    const { image } = this.props;
-    this.attachBIFImage(image);
-
-    this.positionIsCorrected = false;
-    this.state = {
-      style: {},
-    };
-  }
-
-  attachBIFImage(image) {
-    if (this.imageUrl) {
-      URL.revokeObjectURL(this.imageUrl);
-      this.imageUrl = "";
-    }
-
+export default function ImageTip({
+  image,
+  xPosition,
+  className,
+}) {
+  const wrapperEl = useRef(null);
+  const [ imageUrl, setImageUrl ] = useState("");
+  useEffect(() => {
     const blob = new Blob([image], {type: "image/jpeg"});
     const url = URL.createObjectURL(blob);
-    this.imageUrl = url;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.positionIsCorrected = false;
-    const { image } = nextProps;
-    if (this.props.image !== image) {
-      this.attachBIFImage(image);
-    }
-  }
-
-  componentWillUnmount() {
-    URL.revokeObjectURL(this.imageUrl);
-  }
-
-  correctImagePosition() {
-    if (this.positionIsCorrected) {
-      return;
-    }
-    const { xPosition } = this.props;
-
-    if (isNaN(+xPosition) || !this.element) {
+    setImageUrl(url);
+    return () => {
+      URL.revokeObjectURL(url);
+    };
+  }, [image]);
+  useEffect(() => {
+    if (isNaN(+xPosition) || wrapperEl.current === null) {
       return null;
     }
-
-    const style = {
-      transform: `translate(${xPosition}px, -136px)`,
-    };
-
-    this.positionIsCorrected = true;
-    this.setState({ style });
-  }
-
-  componentDidMount() {
-    this.correctImagePosition();
-  }
-
-  componentDidUpdate() {
-    this.correctImagePosition();
-  }
-
-  render() {
-    const { className = "" } = this.props;
-    const { imageUrl } = this;
-    const { style } = this.state;
-
-    return (
-      <div
-        className="image-tip-wrapper"
-        style={style}
-        ref={el => this.element = el}
-      >
-        <img
-          className={"image-tip " + className}
-          src={imageUrl}
-        />
-      </div>
-    );
-  }
+    wrapperEl.current.style.transform = `translate(${xPosition}px, -136px)`;
+  }, [xPosition]);
+  return (
+    <div
+      className="image-tip-wrapper"
+      ref={wrapperEl}
+    >
+      <img
+        className={"image-tip " + className}
+        src={imageUrl}
+      />
+    </div>
+  );
 }
-
-export default ImageTip;
