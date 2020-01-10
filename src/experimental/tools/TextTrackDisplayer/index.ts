d67ded3fcd142b18bba404af22c5150c3ca4369d
peaBerberian <pea.berberian@gmail.com>
2020-01-10 14:58:37 +0100

    tools/TextTrackDisplayer: add compatible text track parsers as exports

--- src/experimental/tools/TextTrackDisplayer/index.ts
+++ src/experimental/tools/TextTrackDisplayer/index.ts
@@ -18,5 +18,11 @@ import TextTrackDisplayer, {
   ISetTextTrackArguments,
 } from "./text_track_displayer";
 
+// -- exported parsers --
+export { HTML_SAMI_PARSER as SAMI_PARSER } from "../../../features/list/html_sami_parser";
+export { HTML_SRT_PARSER as SRT_PARSER } from "../../../features/list/html_srt_parser";
+export { HTML_TTML_PARSER as TTML_PARSER } from "../../../features/list/html_ttml_parser";
+export { HTML_VTT_PARSER as VTT_PARSER } from "../../../features/list/html_vtt_parser";
+
 export default TextTrackDisplayer;
 export { ISetTextTrackArguments };
