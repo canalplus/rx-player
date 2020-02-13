dfb57fbd5e192e82fe059ed60594f07ea7948e53
grenault73 <guillaumerenault@protonmail.com>
2020-02-13 18:06:52 +0100

    compat: update shouldReloadAtEachPeriodChange doc

--- src/compat/should_reload_at_each_period_change.ts
+++ src/compat/should_reload_at_each_period_change.ts
@@ -17,7 +17,14 @@
 import { isSafari } from "./browser_detection";
 
 /**
- * 
+ * On Safari, there can be issues when playing different periods from multiple
+ * contents (e.g. when playing a metaplaylist). Playback may block when
+ * buffering a different content from the current played one. The reasons are
+ * quite unclear. It may depend on media caracteristics changes such as with
+ * framerate and samplerate.
+ *
+ * Then, we should handle one period at a time in the buffer orchestrator and
+ * ask for reloading when changing period.
  */
 const shouldReloadAtEachPeriodChange = isSafari;
 export default shouldReloadAtEachPeriodChange;
