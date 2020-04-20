4cbfc9750e64cd48cdc282c8c0fb10b44682992c
peaBerberian <pea.berberian@gmail.com>
2020-04-20 10:11:51 +0200

    scripts/doc-generator: set better regexp according to CommonMark spec for end of title detection.

--- scripts/doc-generator/read_title_from_md.js
+++ scripts/doc-generator/read_title_from_md.js
@@ -8,11 +8,13 @@
  */
 module.exports = function readTitleFromMD(data) {
   const lines = data.split(/\r\n|\n|\r/);
+
+  // TODO Might be more efficient and less error-prone as a markdown-it plugin.
   for (let i = 0, len = lines.length; i < len; i++) {
     if (/^ *# +/.test(lines[i])) {
       const regExec = /^ *# +(.*)$/.exec(lines[i]);
       if (regExec && regExec[1]) {
-        return regExec[1].replace(/#*$/, "").trim();
+        return regExec[1].replace(/ #* *$/, "").trim();
       }
     }
   }
