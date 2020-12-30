180dcbf17c1db2969440c4550b05213284a77f20
Paul Berberian <pea.berberian@gmail.com>
2020-12-30 13:03:09 +0100

    tests: raise sleep timeout on `initialManifest` test to avoid failure

--- tests/integration/utils/launch_tests_for_content.js
+++ tests/integration/utils/launch_tests_for_content.js
@@ -171,7 +171,7 @@ export default function launchTestsForContent(manifestInfos) {
                              transport,
                              transportOptions: { initialManifest } });
 
-          await sleep(5);
+          await sleep(100);
           expect(xhrMock.getLockedXHR().length).to.be.at.least(1);
           expect(xhrMock.getLockedXHR()[0].url).not.to.equal(manifestInfos.url);
         });
