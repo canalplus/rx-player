6dc4d32f7a07277014dd7635d15cd6afe1647ef2
grenault73 <guillaumerenault@protonmail.com>
2021-03-25 17:00:17 +0100

    manifest: clearer manifest update type definition

--- src/manifest/types.ts
+++ src/manifest/types.ts
@@ -16,14 +16,15 @@
 
 /** Enumerate the different ways a Manifest update can be done. */
 export enum MANIFEST_UPDATE_TYPE {
-  /** The full version of the Manifest has been re-downloaded.
-   * The manifest has been loaded through the same URL used for loading
-   * it originally.
+  /**
+   * Manifest is updated entirely thanks to a re-downloaded version of
+   * the original manifest document.
    */
   Full,
-  /** Only a shortened version of the Manifest has been downloaded.
-   * The manifest has been loaded through the manifest update URL,
-   * which is used only for fetching this version of the manifest.
+  /**
+   * Manifest is updated partially thanks to a shortened version
+   * of the manifest document. The latter's URL might be different
+   * from the original one.
    */
   Partial,
 }
