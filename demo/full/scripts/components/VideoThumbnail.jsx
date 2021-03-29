814c764d919afe007d5ad7c442e0b9df21fb1c2e
Paul Berberian <pea.berberian@gmail.com>
2021-03-29 19:13:49 +0200

    demo: only request a video thumbnail after 40 ms to avoid doing too much requests

--- demo/full/scripts/components/VideoThumbnail.jsx
+++ demo/full/scripts/components/VideoThumbnail.jsx
@@ -14,6 +14,20 @@ import withModulesState from "../lib/withModulesState";
 class VideoThumbnail extends React.Component {
   constructor(...args) {
     super(...args);
+    this.isMounted = true;
+
+    /**
+     * Timeout before loading a thumbnail, to avoid triggering to many requests
+     * in a row.
+     */
+    this._loadThumbnailTimeout = null;
+
+    /**
+     * Timeout before displaying the spinner, for when loading a thumbnail takes
+     * too much time.
+     */
+    this._spinnerTimeout = null;
+
     this.positionIsCorrected = false;
     this.state = {
       style: {},
@@ -55,13 +69,39 @@ class VideoThumbnail extends React.Component {
     this.setState({ style });
   }
 
-  showSpinner() {
-    if (this.state.displaySpinner !== true) {
-      this.setState({ displaySpinner: true });
+  /**
+   * Display a spinner after some delay if `stopSpinnerTimeout` hasn't been
+   * called since.
+   * This function allows to schedule a spinner if the request to display a
+   * thumbnail takes too much time.
+   */
+  startSpinnerTimeoutIfNotAlreadyStarted() {
+    if (this._spinnerTimeout !== null) {
+      return;
     }
+
+    // Wait a little before displaying spinner, to
+    // be sure loading takes time
+    this._spinnerTimeout = setTimeout(() => {
+      this._spinnerTimeout = null;
+      if (this.state.displaySpinner !== true) {
+        this.setState({ displaySpinner: true });
+      }
+    }, 150);
   }
 
+  /**
+   * Hide the spinner if one is active and stop the last started spinner
+   * timeout.
+   * Allow to avoid showing a spinner when the thumbnail we were waiting for
+   * was succesfully loaded.
+   */
   hideSpinner() {
+    if (this._spinnerTimeout !== null) {
+      clearTimeout(this._spinnerTimeout);
+      this._spinnerTimeout = null;
+    }
+
     if (this.state.displaySpinner !== false) {
       this.setState({ displaySpinner: false });
     }
@@ -85,39 +125,48 @@ class VideoThumbnail extends React.Component {
   componentWillUnmount() {
     const { player, attachedVideoThumbnailLoader } = this.props;
     const videoThumbnailLoader = attachedVideoThumbnailLoader;
+    this.hideSpinner();
     if (videoThumbnailLoader) {
       videoThumbnailLoader.dispose();
       player.dispatch("REMOVE_VIDEO_THUMBNAIL_LOADER");
     }
     this._videoElement = undefined;
+    this.isMounted = false;
   }
 
   render() {
     const { style, divSpinnerStyle, spinnerStyle } = this.state;
 
     const videoThumbnailLoader = this.props.attachedVideoThumbnailLoader;
-    if (videoThumbnailLoader) {
-      const { time } = this.props;
-      const roundedTime = Math.round(time);
-      let spinnerTimeout;
-      // Only show spinner when time has changed
-      if (this.lastSetTime !== roundedTime) {
-        // Wait a little before displaying spinner, to
-        // be sure loading takes time
-        spinnerTimeout = setTimeout(() => {
-          this.showSpinner();
-        }, 300);
+
+    const { time } = this.props;
+    const roundedTime = Math.round(time);
+
+    if (videoThumbnailLoader && this.lastSetTime !== roundedTime) {
+      this.startSpinnerTimeoutIfNotAlreadyStarted();
+
+      if (this._loadThumbnailTimeout !== null) {
+        clearTimeout(this._loadThumbnailTimeout);
       }
-      this.lastSetTime = roundedTime;
-      videoThumbnailLoader.setTime(roundedTime)
-        .then(() => {
-          clearTimeout(spinnerTimeout);
-          this.hideSpinner();
-        })
-        .catch(() => {
-          clearTimeout(spinnerTimeout);
-          this.hideSpinner();
-        });
+
+      // load thumbnail after a 40ms timer to avoid doing too many requests
+      // when the user quickly moves its pointer or whatever is calling this
+      this._loadThumbnailTimeout = setTimeout(() => {
+        this._loadThumbnailTimeout = null;
+        videoThumbnailLoader.setTime(roundedTime)
+          .then(() => {
+            if (time !== this.props.time || !this.isMounted) {
+              return;
+            }
+            this.hideSpinner();
+          })
+          .catch(() => {
+            if (time !== this.props.time || !this.isMounted) {
+              return;
+            }
+            this.hideSpinner();
+          });
+      }, 40);
     }
 
     const divToDisplay = <div
