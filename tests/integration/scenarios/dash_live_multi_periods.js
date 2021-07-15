a9fbfb29b527c24c14c71bf45b885a20ae002dc6
Paul Berberian <pea.berberian@gmail.com>
2021-07-15 11:55:06 +0200

    remove methods: getManifest, getCurrentAdaptations and getCurrentRepresentations
    as they reveal the RxPlayer's internals too much

--- tests/integration/scenarios/dash_live_multi_periods.js
+++ tests/integration/scenarios/dash_live_multi_periods.js
@@ -52,11 +52,6 @@ describe("DASH live content multi-periods (SegmentTemplate)", function() {
     await xhrMock.flush();
     await sleepWithoutSinonStub(1);
 
-    const manifest = player.getManifest();
-    expect(manifest).not.to.equal(null);
-    const { periods } = manifest;
-
-    expect(periods.length).to.equal(3);
     const now = 1567781280 + 500;
     const maxPos = player.getMaximumPosition();
     expect(maxPos).to.be.closeTo(now, 2);
@@ -75,38 +70,8 @@ describe("DASH live content multi-periods (SegmentTemplate)", function() {
     await xhrMock.flush();
     await sleepWithoutSinonStub(1);
 
-    const manifest = player.getManifest();
-    expect(manifest).not.to.equal(null);
-    const { periods } = manifest;
-
-    expect(periods.length).to.equal(3);
     const now = 1567781280 + 500;
     const minPos = player.getMinimumPosition();
     expect(minPos).to.be.closeTo(now - manifestInfos.tsbd, 2);
-  });
-
-  it("should correclty parse manifest and periods boundaries", async () => {
-    xhrMock.lock();
-
-    player.loadVideo({
-      url: manifestInfos.url,
-      transport: manifestInfos.transport,
-    });
-
-    await sleepWithoutSinonStub(1);
-    expect(xhrMock.getLockedXHR().length).to.equal(1);
-    await xhrMock.flush();
-    await sleepWithoutSinonStub(1);
-
-    const manifest = player.getManifest();
-    expect(manifest).not.to.equal(null);
-    const { periods } = manifest;
-
-    expect(periods[0].start).to.equal(1567780920);
-    expect(periods[0].end).to.equal(1567781100);
-    expect(periods[1].start).to.equal(1567781100);
-    expect(periods[1].end).to.equal(1567781280);
-    expect(periods[2].start).to.equal(1567781280);
-    expect(periods[2].end).to.equal(undefined);
   });
 });
