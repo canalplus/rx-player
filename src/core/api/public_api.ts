67f47cbb6ac79637c0f96b8fe008297e391c28d2
Paul Berberian <pea.berberian@gmail.com>
2021-07-16 14:39:32 +0200

    rename getVideoBufferGap into getCurrentBufferGap

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -1154,7 +1154,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
    *   - the current time
    * @returns {Number}
    */
-  getVideoBufferGap() : number {
+  getCurrentBufferGap() : number {
     if (this.videoElement === null) {
       throw new Error("Disposed player");
     }
