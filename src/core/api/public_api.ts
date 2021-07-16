b2ba6ee1bd175e3d2dff424add44475cdfdad0b9
Paul Berberian <pea.berberian@gmail.com>
2021-07-16 14:34:56 +0200

    rename getVideoDuration into getMediaDuration

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -1091,7 +1091,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
    * NaN if no video is playing.
    * @returns {Number}
    */
-  getVideoDuration() : number {
+  getMediaDuration() : number {
     if (this.videoElement === null) {
       throw new Error("Disposed player");
     }
