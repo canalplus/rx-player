244095d98e2f529106dd049bfe7fedc33d956a69
Paul Berberian <pea.berberian@gmail.com>
2021-07-16 14:34:56 +0200

    rename getVideoDuration into getMediaDuration

--- tests/integration/utils/launch_tests_for_content.js
+++ tests/integration/utils/launch_tests_for_content.js
@@ -587,7 +587,7 @@ export default function launchTestsForContent(manifestInfos) {
       });
     });
 
-    describe("getVideoDuration", () => {
+    describe("getMediaDuration", () => {
       if (isLive) {
         it("should return Math.MAX_NUMBER", async () => {
           player.loadVideo({
@@ -596,7 +596,7 @@ export default function launchTestsForContent(manifestInfos) {
             autoPlay: false,
           });
           await waitForLoadedStateAfterLoadVideo(player);
-          expect(player.getVideoDuration()).to.equal(Math.MAX_NUMBER);
+          expect(player.getMediaDuration()).to.equal(Math.MAX_NUMBER);
         });
       } else {
         it("should return the duration of the whole video", async () => {
@@ -606,7 +606,7 @@ export default function launchTestsForContent(manifestInfos) {
             autoPlay: false,
           });
           await waitForLoadedStateAfterLoadVideo(player);
-          expect(player.getVideoDuration()).to.be.closeTo(maximumPosition, 0.1);
+          expect(player.getMediaDuration()).to.be.closeTo(maximumPosition, 0.1);
         });
       }
     });
