b979022b3099fbb43b8b846a32da0eb671b2b8e6
Paul Berberian <pea.berberian@gmail.com>
2021-08-09 17:38:49 +0200

    periodChange events do not emit the full Period structure

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -56,7 +56,7 @@ import {
   IKeySystemConfigurationOutput,
   IKeySystemOption,
   ILoadVideoOptions,
-  IPeriod,
+  IPeriodChangeEvent,
   IPlayerError,
   IPlayerState,
   IPositionUpdate,
@@ -2362,7 +2362,11 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     const cancelSignal = contentInfos.currentContentCanceller.signal;
     if (this._priv_contentEventsMemory.periodChange !== period) {
       this._priv_contentEventsMemory.periodChange = period;
-      this._priv_triggerEventIfNotStopped("periodChange", period, cancelSignal);
+      this.trigger("periodChange", { start: period.start, end: period.end });
+      this._priv_triggerEventIfNotStopped("periodChange",
+                                          { start: period.start,
+                                            end: period.end },
+                                          cancelSignal);
     }
 
     this._priv_triggerEventIfNotStopped("availableAudioTracksChange",
@@ -2875,7 +2879,7 @@ interface IPublicAPIEvent {
   volumeChange : number;
   error : IPlayerError | Error;
   warning : IPlayerError | Error;
-  periodChange : IPeriod;
+  periodChange : IPeriodChangeEvent;
   availableAudioBitratesChange : number[];
   availableVideoBitratesChange : number[];
   availableAudioTracksChange : IAvailableAudioTrack[];
