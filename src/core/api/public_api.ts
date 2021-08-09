f2ffe256dea6fe1520a0813b5cb4af2d806cfd74
Paul Berberian <pea.berberian@gmail.com>
2021-08-09 17:33:01 +0200

    re-renamed getMediaElement to getVideoElement

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -1057,7 +1057,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
    * @returns {HTMLMediaElement|null} - The HTMLMediaElement used (`null` when
    * disposed)
    */
-  getMediaElement() : HTMLMediaElement|null {
+  getVideoElement() : HTMLMediaElement|null {
     return this.videoElement;
   }
 
