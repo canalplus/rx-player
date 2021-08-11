58c94ff990a630fd3265b2f57faf8877c05a2ff8
Paul Berberian <pea.berberian@gmail.com>
2021-08-11 18:48:42 +0200

    finalize new version of the segmentLoader API

--- src/transports/dash/segment_loader.ts
+++ src/transports/dash/segment_loader.ts
@@ -214,10 +214,11 @@ export default function generateSegmentLoader(
 
       const customCallbacks = { reject, resolve, progress, fallback };
 
-      const args = { context: { segment: context.segment,
-                                type: context.type },
+      const args = { isInit: context.segment.isInit,
                      timeout: options.timeout,
-                     transport: "dash",
+                     range: context.segment.range,
+                     indexRange: context.segment.indexRange,
+                     type: context.type,
                      url };
       const abort = customSegmentLoader(args, customCallbacks);
 
