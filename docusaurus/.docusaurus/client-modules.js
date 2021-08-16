ee77d1dd0d86629ee7b77a26d195e8963a53ab12
Paul Rosset <paulrosset96@gmail.com>
2021-08-16 14:45:17 +0200

    rename docs folder

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/prosset/Documents/Canal+/rx-player-c+/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/prosset/Documents/Canal+/rx-player-c+/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/prosset/Documents/Canal+/rx-player-c+/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/prosset/Documents/Canal+/rx-player-c+/node_modules/infima/dist/css/default/default.css"),
-  require("/home/prosset/Documents/Canal+/rx-player-c+/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/prosset/Documents/Canal+/rx-player-c+/docusaurus/src/css/custom.css"),
-];
