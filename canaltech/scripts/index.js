68cfbc00a116ff8ee245e7b27c735892543ca2cb
Paul Berberian <pea.berberian@gmail.com>
2022-04-08 14:26:14 +0200

    gh-pages: remove most unused pages

--- canaltech/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import "@babel/polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./controllers/Main.jsx";
-
-ReactDOM.render(<Main />, document.getElementById("player-container"));
