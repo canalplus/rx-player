64c8a6341e7bb4fab9e960f985e49814b4d04d9c
Paul Berberian <pea.berberian@gmail.com>
2022-04-15 15:49:05 +0200

    Avoid emitting newAvalailablePeriods event if TracksStore is disposed

--- src/core/api/track_management/tracks_store.ts
+++ src/core/api/track_management/tracks_store.ts
@@ -243,7 +243,9 @@ export default class TracksStore extends EventEmitter<ITracksStoreEvents> {
       }
       return acc;
     }, []);
-    this.trigger("newAvailablePeriods", periodsAdded);
+    if (periodsAdded.length > 0) {
+      this.trigger("newAvailablePeriods", periodsAdded);
+    }
   }
 
   /**
@@ -889,7 +891,13 @@ export default class TracksStore extends EventEmitter<ITracksStoreEvents> {
   }
 
   public dispose() : void {
-    this.resetPeriodObjects();
+    while (true) {
+      const lastPeriod = this._storedPeriodInfo.pop();
+      if (lastPeriod === undefined) {
+        return;
+      }
+      lastPeriod.isRemoved = true;
+    }
   }
 
   /**
