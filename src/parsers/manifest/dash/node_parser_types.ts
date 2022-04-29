036ffc8fc8f36fa62ce65c50999038229dd861b5
Achraf <achraf.laamoum@gmail.com>
2022-04-29 11:18:00 +0200

    feat/ add label parsing in WASM

--- src/parsers/manifest/dash/node_parser_types.ts
+++ src/parsers/manifest/dash/node_parser_types.ts
@@ -234,6 +234,7 @@ export interface IAdaptationSetAttributes {
   width? : number;
   availabilityTimeComplete?: boolean;
   availabilityTimeOffset?: number;
+  label?: string;
 }
 
 export interface IRepresentationIntermediateRepresentation {
