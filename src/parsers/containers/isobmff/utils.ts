408191eb346e562e587a1ce097ba1112daf5c5d3
Paul Berberian <pea.berberian@gmail.com>
2022-05-23 11:34:58 +0200

    [wip] create fake encryption information to work-around PlayReady issues

--- src/parsers/containers/isobmff/utils.ts
+++ src/parsers/containers/isobmff/utils.ts
@@ -22,6 +22,7 @@ import {
   be4toi,
   be8toi,
   concat,
+  itobe2,
   itobe4,
   itobe8,
 } from "../../../utils/byte_parsing";
@@ -33,12 +34,278 @@ import { getPlayReadyKIDFromPrivateData } from "./drm";
 import { getBoxContent, getBoxOffsets, getChildBox } from "./get_box";
 import { getEMSG, getMDIA, getTRAF, getTRAFs } from "./read";
 
+const AVC1_BOX_NAME = 0x61766331;
+const AVC3_BOX_NAME = 0x61766333;
+const AC3_BOX_NAME = 0x61632D33;
+const EC3_BOX_NAME = 0x65632D33;
+const MP4A_BOX_NAME = 0x6D703461;
+
 /** Information related to a PSSH box. */
 export interface IISOBMFFPSSHInfo {
   /** Corresponding DRM's system ID, as an hexadecimal string. */
   systemId: string;
   /** Additional data contained in the PSSH Box. */
   privateData: Uint8Array;
+}
+
+// XXX TODO move to compat?
+/**
+ * Fake `sinf` box containing dummy encryption data.
+ * @see fakeEncryptionDataInInitSegment
+ */
+const FAKE_SINF_BOX = new Uint8Array([
+  0x00, 0x00, 0x00, 0x50, // Length
+  0x73, 0x69, 0x6e, 0x66, // `sinf`
+
+  // -- Start of `frma`
+
+  0x00, 0x00, 0x00, 0x0c, // Length of `frma` box
+  0x66, 0x72, 0x6d, 0x61, // `frma`
+  0x00, 0x00, 0x00, 0x00, // format in `frma` (empty for now, but will be dynamically
+                          // updated)
+
+  // -- End of `frma`
+
+  // -- Start of `schm`
+
+  0x00, 0x00, 0x00, 0x14, // Length of `schm` box
+  0x73, 0x63, 0x68, 0x6d, // `schm`
+  0x00, 0x00, 0x00, 0x00, // This is a ISOBMFF "FullBox" so, there are versions
+                          // and flags we can just set all of it to `0` here.
+  0x63, 0x65, 0x6e, 0x63, // Scheme. Here `cenc`
+  0x00, 0x01, 0x00, 0x00, // Version of scheme Here 1.0
+
+  // -- End of `schm`
+
+  // -- Start of `schi`
+
+  0x00, 0x00, 0x00, 0x28, // Length of the `schi` box
+  0x73, 0x63, 0x68, 0x69, // `schi`
+
+  // -- Start of `tenc`
+
+  0x00, 0x00, 0x00, 0x20, // Length of the `tenc` box
+  0x74, 0x65, 0x6e, 0x63, // `tenc`
+  0x00, 0x00, 0x00, 0x00, // This is a ISOBMFF "FullBox" so, there are versions
+                          // and flags we can just set all of it to `0` here.
+  0x00, 0x00,             // Reserved bits we can just set to `0`
+  0x01,                   // Default protected
+  0x08,                   // Default per-sample IV size
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Key id. As we just want to create
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // a fake one, we just set all 0s.
+
+  // -- End of `tenc`, `schi` and the whole `sinf`
+]);
+
+/**
+ * Fake encryption metadata in the given initialization segment to make the
+ * browser believe it is handling an encrypted segment.
+ *
+ * This beautiful mess is needed because multiple user-agents (meaning "browser"
+ * in the w3c dialect :p) implementations, mostly those based on PlayReady,
+ * poorly handle contents with mixed encrypted and unencrypted contents.
+ *
+ * By faking encryption information in initialization segments, it seems that
+ * most of those problems disappear.
+ *
+ * Note that this work-around was not found by me, I actually saw it in the
+ * shaka-player's code (google's own DASH player) while investigating this issue.
+ * So kudos and thanks to them I guess!
+ *
+ * @param {BufferSource} segment
+ * @returns {Uint8Array}
+ */
+export function fakeEncryptionDataInInitSegment(
+  segment : BufferSource
+) : Uint8Array {
+  // Format it into an Uint8Array
+  const buf = segment instanceof Uint8Array ?  segment :
+              segment instanceof ArrayBuffer ? new Uint8Array(segment) :
+                                               new Uint8Array(segment.buffer);
+
+  /**
+   * Contains offsets of all encountered boxes that should be the parents of the
+   * box we want to modify. Will be needed later to update their length.
+   */
+  const parentBoxesOffsets = [];
+
+  // Recuperate those offsets:
+  {
+    const parentBoxes = [ 0x6D6F6F76 /* moov */,
+                          0x7472616B /* trak */,
+                          0x6D646961 /* mdia */,
+                          0x6D696E66 /* minf */,
+                          0x7374626C /* stbl */,
+                          0x73747364 /* stsd */];
+    let currBox = buf;
+    let relativeOffset = 0;
+    for (const parentBoxName of parentBoxes) {
+      const offsets = getBoxOffsets(currBox, parentBoxName);
+      if (offsets === null) {
+        return buf;
+      }
+      currBox = currBox.subarray(offsets[1], offsets[2]);
+      parentBoxesOffsets.push(offsets.map(offset => offset + relativeOffset));
+      relativeOffset += offsets[1];
+    }
+  }
+
+  const stsdOffsets = parentBoxesOffsets[parentBoxesOffsets.length - 1];
+  const stsdSubBoxesStart = stsdOffsets[1] + 8;
+  const stsdSubBoxes = buf.subarray(stsdSubBoxesStart, stsdOffsets[2]);
+  const encv = getBoxContent(stsdSubBoxes, 0x656E6376 /* encv */);
+  const enca = getBoxContent(stsdSubBoxes, 0x656E6361 /* enca */);
+  if (encv !== null || enca !== null) {
+    // There's already encryption data here
+    return buf;
+  }
+
+  interface ISampleEntryBoxInfo {
+    /** Whether the corresponding box is for audio or video data. */
+    type: "audio" | "video";
+
+    /** Name of the parsed box. */
+    name: number;
+
+    /**
+     * Offsets in bytes, relative to the first inner box of the parent
+     * `stsd` box.
+     * The first number indicates the offset of the first byte from
+     * that box, the second, the offset of the data (after the size and
+     * name of the box), and the third: the end - not included - of the
+     * box.
+     */
+    relativeOffsets: [number, number, number];
+  }
+
+  /** Information about every boxes we need to add encryption metadata to. */
+  const boxesToUpdate : ISampleEntryBoxInfo[] = [
+    { type: "video" as const,
+      name: AVC1_BOX_NAME,
+      relativeOffsets: getBoxOffsets(stsdSubBoxes, AVC1_BOX_NAME) },
+    { type: "video" as const,
+      name: AVC3_BOX_NAME,
+      relativeOffsets: getBoxOffsets(stsdSubBoxes, AVC3_BOX_NAME) },
+    { type: "audio" as const,
+      name: AC3_BOX_NAME,
+      relativeOffsets: getBoxOffsets(stsdSubBoxes, AC3_BOX_NAME) },
+    { type: "audio" as const,
+      name: EC3_BOX_NAME,
+      relativeOffsets: getBoxOffsets(stsdSubBoxes, EC3_BOX_NAME) },
+    { type: "audio" as const,
+      name: MP4A_BOX_NAME,
+      relativeOffsets: getBoxOffsets(stsdSubBoxes, MP4A_BOX_NAME) },
+    // TODO Does that also covers hvc segments and other rarer codecs?
+    // If it doesn't, it needs to be added there.
+  ].filter((b) : b is ISampleEntryBoxInfo => b.relativeOffsets !== null);
+
+  // Sort from last to first to simplify length updates
+  boxesToUpdate.sort((a, b) => b.relativeOffsets[0] - a.relativeOffsets[0]);
+
+  /** The segment that will be returned in the end, which will be updated here. */
+  let updatedSeg = buf;
+  for (const box of boxesToUpdate) {
+    // Create an Uint8Array which will contain both the previous content of that
+    // box and our new fake encryption data
+    const boxLen = box.relativeOffsets[2] - box.relativeOffsets[0];
+    let newBox = new Uint8Array(boxLen + FAKE_SINF_BOX.length);
+
+    // We put the content of `box` at the beginning. We will replace this box name
+    // later, as described in the ISOBMFF spec for encryption metadata.
+    const boxContent = stsdSubBoxes.subarray(box.relativeOffsets[0],
+                                             box.relativeOffsets[2]);
+    newBox.set(boxContent, 0);
+
+    /**
+     * Offset where the initial box's title is in `newBox`.
+     * We prefer starting at the content offset and then descending 4 bytes
+     * instead of going from the start, because here names are always 4 bytes
+     * though length may technically be expressed in more than 4.
+     */
+    const titleOffsetForBox = box.relativeOffsets[1] - box.relativeOffsets[0] - 4;
+    if (box.type === "audio") {
+      // Put "enca" in place of original box name, as indicated by
+      // `8.12 Support for Protected Streams` of ISO/IEC 14496-12
+      newBox.set(itobe4(0x656E6361), titleOffsetForBox);
+    } else {
+      // Put "encv" in place of original box name, as indicated by
+      // `8.12 Support for Protected Streams` of ISO/IEC 14496-12
+      newBox.set(itobe4(0x656E6376), titleOffsetForBox);
+    }
+    newBox.set(FAKE_SINF_BOX, boxLen); // Put our fake box after
+
+    const sinfOffsets = getBoxOffsets(newBox, 0x73696E66 /* sinf */);
+    if (sinfOffsets === null) {
+      log.warn("ISOBMFF: sinf not found, this should not be possible.");
+      return buf;
+    }
+    const sinfContent = newBox.subarray(sinfOffsets[1], sinfOffsets[2]);
+    const frmaOffsets = getBoxOffsets(sinfContent, 0x66726D61 /* frma */)
+      ?.map(val => val + sinfOffsets[1]);
+    if (frmaOffsets === undefined) {
+      log.warn("ISOBMFF: frma not found in sinf, this should not be possible.");
+      return buf;
+    }
+
+    // Put original box name as value of frma, again as indicated by the spec
+    newBox.set(itobe4(box.name), frmaOffsets[1]);
+    newBox = updateBoxLength(newBox);
+
+    const previousUpdatedSeg = updatedSeg;
+
+    // Grow the segment we work with to include our new box, we will update it just after.
+    updatedSeg = new Uint8Array(previousUpdatedSeg.length + newBox.byteLength);
+
+    // For Xbox One, we cut and insert at the start of the source box.  For
+    // other platforms, we cut and insert at the end of the source box.  It's
+    // not clear why this is necessary on Xbox One, but it seems to be evidence
+    // of another bug in the firmware implementation of MediaSource & EME.
+    // XXX TODO
+    // const cutPoint = shaka.util.Platform.isXkboxOne() ?
+    //     sourceBox.start :
+    //     sourceBox.start + sourceBox.size;
+
+    /**
+     * The absolute end offset of `box` in the init segment given originally.
+     *
+     * We can still rely on that original value because we're iterating on inner
+     * boxes in a reverse order. Thus, we are never moving further not-yet processed
+     * boxes.
+     */
+    const absoluteBoxEnd = stsdSubBoxesStart + box.relativeOffsets[2];
+
+    // Let everything coming before `box` as is (for now, lengths will be updated later).
+    updatedSeg.set(previousUpdatedSeg.subarray(0, absoluteBoxEnd), 0);
+
+    // Move everything from end of `box` to the end of the bigger segment, so
+    // the new box can be put before
+    updatedSeg.set(previousUpdatedSeg.subarray(absoluteBoxEnd),
+                   absoluteBoxEnd + newBox.length);
+
+    // and put the new box in between
+    updatedSeg.set(newBox, absoluteBoxEnd);
+
+    // The parents up the chain from the encryption metadata box need their
+    // sizes adjusted to account for the added box. These offsets should not be
+    // changed, because they should all be within the first section we copy.
+    for (const parentBoxesOffset of parentBoxesOffsets) {
+      const parentBox = updatedSeg.subarray(parentBoxesOffset[0],
+                                            parentBoxesOffset[2] + newBox.length);
+
+      // TODO This will break if `updateBoxLength` actually needs to create a
+      // new box, for example because it might have to create an enlarged length
+      // box if it cannot be expressed in 32 bits.
+      // This is however very very rare (never actually seen?), and it
+      // represents much more work, so I just don't care for now.
+      updateBoxLength(parentBox);
+    }
+
+    let nbStsdEntries = be2toi(updatedSeg, stsdOffsets[1] + 6);
+    nbStsdEntries += 1;
+    updatedSeg.set(itobe2(nbStsdEntries), stsdOffsets[1] + 6);
+  }
+
+  return updatedSeg;
 }
 
 /**
