a558bdca0e5a11986596d5a9c6e92587459030c6
Paul Berberian <pea.berberian@gmail.com>
2022-06-22 10:22:42 +0200

    Add lost tracks events

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -43,18 +43,24 @@ import Manifest, {
 } from "../../manifest";
 import {
   IAudioRepresentation,
-  ITrackType,
   IAudioRepresentationsSwitchingMode,
   IAudioTrack,
+  IAudioTrackSetting,
   IAudioTrackSwitchingMode,
   IAvailableAudioTrack,
   IAvailableTextTrack,
   IAvailableVideoTrack,
   IBitrateEstimate,
+  IBrokenRepresentationsLockContext,
   IConstructorOptions,
   IDecipherabilityUpdateContent,
   IKeySystemConfigurationOutput,
   ILoadVideoOptions,
+  ILockedAudioRepresentationsProperties,
+  ILockedVideoRepresentationsProperties,
+  ILostAudioTrackEventPayload,
+  ILostTextTrackEventPayload,
+  ILostVideoTrackEventPayload,
   IPeriod,
   IPeriodChangeEvent,
   IPlayerError,
@@ -63,8 +69,10 @@ import {
   IStreamEvent,
   ITextTrack,
   IVideoRepresentation,
+  ITextTrackSetting,
   IVideoRepresentationsSwitchingMode,
   IVideoTrack,
+  IVideoTrackSetting,
   IVideoTrackSwitchingMode,
 } from "../../public_types";
 import arrayIncludes from "../../utils/array_includes";
@@ -1599,6 +1607,14 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     return getCurrentKeySystem(this.videoElement);
   }
 
+  getCurrentPeriod() : IPeriod | null {
+    const currentPeriod = this._priv_contentInfos?.currentPeriod;
+    if (isNullOrUndefined(currentPeriod)) {
+      return null;
+    }
+    return { id: currentPeriod.id, start: currentPeriod.start, end: currentPeriod.end };
+  }
+
   /**
    * Returns both the name of the key system (e.g. `"com.widevine.alpha"`) and
    * the `MediaKeySystemConfiguration` currently associated to the
@@ -1805,7 +1821,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
 
   /**
    * Update the text language for the current Period.
-   * @param {string | Object} textId
+   * @param {string | Object} arg
    * @throws Error - the current content has no TracksStore.
    * @throws Error - the given id is linked to no text track.
    */
@@ -1860,7 +1876,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
 
   /**
    * Update the video track for the current Period.
-   * @param {string | Object} videoId
+   * @param {string | Object} arg
    * @throws Error - the current content has no TracksStore.
    * @throws Error - the given id is linked to no video track.
    */
@@ -2224,12 +2240,21 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     contentInfos.tracksStore.addEventListener("brokenRepresentationsLock", (e) => {
       this.trigger("brokenRepresentationsLock", e);
     });
+    contentInfos.tracksStore.addEventListener("lostVideoTrack", (e) => {
+      this.trigger("lostVideoTrack", e);
+    });
+    contentInfos.tracksStore.addEventListener("lostAudioTrack", (e) => {
+      this.trigger("lostAudioTrack", e);
+    });
+    contentInfos.tracksStore.addEventListener("lostTextTrack", (e) => {
+      this.trigger("lostTextTrack", e);
+    });
 
     contentInfos.tracksStore.updatePeriodList(manifest);
 
     manifest.addEventListener("manifestUpdate", (updates) => {
       // Update the tracks chosen if it changed
-      if (contentInfos.tracksStore !== null) {
+      if (!isNullOrUndefined(contentInfos?.tracksStore)) {
         contentInfos.tracksStore.updatePeriodList(manifest);
       }
       const currentPeriod = this._priv_contentInfos?.currentPeriod ?? undefined;
@@ -2754,8 +2779,10 @@ interface IPublicAPIEvent {
   play: null;
   pause: null;
   newAvailablePeriods : IPeriod[];
-  brokenRepresentationsLock : { period : IPeriod;
-                                trackType : ITrackType; };
+  brokenRepresentationsLock : IBrokenRepresentationsLockContext;
+  lostVideoTrack : ILostVideoTrackEventPayload;
+  lostAudioTrack : ILostAudioTrackEventPayload;
+  lostTextTrack : ILostTextTrackEventPayload;
   seeking : null;
   seeked : null;
   streamEvent : IStreamEvent;
@@ -2821,37 +2848,6 @@ interface IPublicApiContentInfos {
    * has no MediaElementTracksStore.
    */
   mediaElementTracksStore : MediaElementTracksStore|null;
-}
-
-export interface ILockedVideoRepresentationsProperties {
-  representations : string[];
-  periodId? : string | undefined;
-  switchingMode? : IVideoRepresentationsSwitchingMode | undefined;
-}
-
-export interface ILockedAudioRepresentationsProperties {
-  representations : string[];
-  periodId? : string | undefined;
-  switchingMode? : IAudioRepresentationsSwitchingMode | undefined;
-}
-
-export interface IAudioTrackSetting {
-  trackId : string;
-  periodId? : string | undefined;
-  switchingMode? : IAudioTrackSwitchingMode | undefined;
-  lockedRepresentations? : string[] | undefined;
-}
-
-export interface IVideoTrackSetting {
-  trackId : string;
-  periodId? : string | undefined;
-  switchingMode? : IVideoTrackSwitchingMode | undefined;
-  lockedRepresentations? : string[] | undefined;
-}
-
-export interface ITextTrackSetting {
-  trackId : string;
-  periodId? : string | undefined;
 }
 
 export default Player;
