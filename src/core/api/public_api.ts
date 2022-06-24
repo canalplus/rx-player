0d20dee238bbb2138702d8a0a22ffbf9e2d25e1c
Paul Berberian <pea.berberian@gmail.com>
2022-06-24 10:09:01 +0200

    Replace lostTrack events by autoTrackSwitch

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -58,9 +58,7 @@ import {
   ILoadVideoOptions,
   ILockedAudioRepresentationsProperties,
   ILockedVideoRepresentationsProperties,
-  ILostAudioTrackEventPayload,
-  ILostTextTrackEventPayload,
-  ILostVideoTrackEventPayload,
+  IAutoTrackSwitchEventPayload,
   IPeriod,
   IPeriodChangeEvent,
   IPlayerError,
@@ -2088,14 +2086,8 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     contentInfos.tracksStore.addEventListener("brokenRepresentationsLock", (e) => {
       this.trigger("brokenRepresentationsLock", e);
     });
-    contentInfos.tracksStore.addEventListener("lostVideoTrack", (e) => {
-      this.trigger("lostVideoTrack", e);
-    });
-    contentInfos.tracksStore.addEventListener("lostAudioTrack", (e) => {
-      this.trigger("lostAudioTrack", e);
-    });
-    contentInfos.tracksStore.addEventListener("lostTextTrack", (e) => {
-      this.trigger("lostTextTrack", e);
+    contentInfos.tracksStore.addEventListener("autoTrackSwitch", (e) => {
+      this.trigger("autoTrackSwitch", e);
     });
 
     contentInfos.tracksStore.updatePeriodList(manifest);
@@ -2604,7 +2596,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     if (this._priv_contentInfos === null ||
         this._priv_contentInfos.tracksStore === null)
     {
-      log.warn("API: Trying to call TracksStore method while not created");
+      log.warn("API: Trying to call track API too soon");
       return defaultValue;
     }
     const { tracksStore } = this._priv_contentInfos;
@@ -2645,9 +2637,7 @@ interface IPublicAPIEvent {
   decipherabilityUpdate : IDecipherabilityUpdateContent[];
   newAvailablePeriods : IPeriod[];
   brokenRepresentationsLock : IBrokenRepresentationsLockContext;
-  lostVideoTrack : ILostVideoTrackEventPayload;
-  lostAudioTrack : ILostAudioTrackEventPayload;
-  lostTextTrack : ILostTextTrackEventPayload;
+  autoTrackSwitch : IAutoTrackSwitchEventPayload;
   seeking : null;
   seeked : null;
   streamEvent : IStreamEvent;
