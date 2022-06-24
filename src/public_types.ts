9aa78dc2d7f6b3fcb624a39d4de0f05f4047965e
Paul Berberian <pea.berberian@gmail.com>
2022-06-24 10:09:01 +0200

    Replace lostTrack events by autoTrackSwitch

--- src/public_types.ts
+++ src/public_types.ts
@@ -839,16 +839,11 @@ export interface IBrokenRepresentationsLockContext {
   trackType : ITrackType;
 }
 
-export interface ILostVideoTrackEventPayload {
+export interface IAutoTrackSwitchEventPayload {
   period : IPeriod;
-}
-
-export interface ILostAudioTrackEventPayload {
-  period : IPeriod;
-}
-
-export interface ILostTextTrackEventPayload {
-  period : IPeriod;
+  trackType : IAdaptationType;
+  reason : "missing" |
+           string;
 }
 
 export interface ILockedVideoRepresentationsProperties {
