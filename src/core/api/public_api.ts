4a322ee46f292f4c22f2cbc6f077da64508e942f
Paul Berberian <pea.berberian@gmail.com>
2022-08-05 16:26:17 +0200

    doc: update API documentation to better align with v4 tracks and representations features

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -59,8 +59,8 @@ import {
   IDecipherabilityUpdateContent,
   IKeySystemConfigurationOutput,
   ILoadVideoOptions,
-  ILockedAudioRepresentationsProperties,
-  ILockedVideoRepresentationsProperties,
+  ILockedAudioRepresentationsSettings,
+  ILockedVideoRepresentationsSettings,
   IAutoTrackSwitchEventPayload,
   IPeriod,
   IPeriodChangeEvent,
@@ -1945,7 +1945,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
   }
 
   lockVideoRepresentations(
-    arg : string[] | ILockedVideoRepresentationsProperties
+    arg : string[] | ILockedVideoRepresentationsSettings
   ) : void {
     if (this._priv_contentInfos === null) {
       throw new Error("No content loaded");
@@ -1975,7 +1975,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
   }
 
   lockAudioRepresentations(
-    arg : string[] | ILockedAudioRepresentationsProperties
+    arg : string[] | ILockedAudioRepresentationsSettings
   ) : void {
     if (this._priv_contentInfos === null) {
       throw new Error("No content loaded");
