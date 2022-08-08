f182efaeec29b7f46f3cb78f779e869fa867b9bc
Paul Berberian <pea.berberian@gmail.com>
2022-08-08 17:57:32 +0200

    Remove manualBitrateSwitchingMode option

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -623,7 +623,6 @@ class Player extends EventEmitter<IPublicAPIEvent> {
             initialManifest,
             keySystems,
             lowLatencyMode,
-            manualBitrateSwitchingMode,
             minimumManifestUpdateInterval,
             networkConfig,
             onCodecSwitch,
@@ -783,7 +782,6 @@ class Player extends EventEmitter<IPublicAPIEvent> {
           textTrackElement: options.textTrackElement };
 
       const bufferOptions = objectAssign({ enableFastSwitching,
-                                           manualBitrateSwitchingMode,
                                            onCodecSwitch },
                                          this._priv_bufferOptions);
 
