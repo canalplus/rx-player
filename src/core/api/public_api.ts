f69011a44623a09cc7ca2ab7c4d0c90440ca1be0
Paul Berberian <pea.berberian@gmail.com>
2022-08-22 16:34:41 +0200

    [V4] Flatten transportOptions loadVideo option

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -569,7 +569,14 @@ class Player extends EventEmitter<IPublicAPIEvent> {
             onCodecSwitch,
             startAt,
             transport,
-            transportOptions,
+            checkMediaSegmentIntegrity,
+            manifestLoader,
+            manifestUpdateUrl,
+            referenceDateTime,
+            representationFilter,
+            segmentLoader,
+            serverSyncInfos,
+            __priv_patchLastSegmentInSidx,
             url } = options;
 
     // Perform multiple checks on the given options
@@ -601,7 +608,15 @@ class Player extends EventEmitter<IPublicAPIEvent> {
         throw new Error("MediaSource streaming not supported");
       }
 
-      const transportPipelines = transportFn(transportOptions);
+      const transportPipelines = transportFn({ lowLatencyMode,
+                                               checkMediaSegmentIntegrity,
+                                               manifestLoader,
+                                               manifestUpdateUrl,
+                                               referenceDateTime,
+                                               representationFilter,
+                                               segmentLoader,
+                                               serverSyncInfos,
+                                               __priv_patchLastSegmentInSidx });
 
       const { offlineRetry,
               segmentRetry,
