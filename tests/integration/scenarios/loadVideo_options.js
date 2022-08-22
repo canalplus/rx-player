85715b6b90aec44ea8786033d3be42f5d6165a7e
Paul Berberian <pea.berberian@gmail.com>
2022-08-22 16:34:41 +0200

    [V4] Flatten transportOptions loadVideo option

--- tests/integration/scenarios/loadVideo_options.js
+++ tests/integration/scenarios/loadVideo_options.js
@@ -23,7 +23,7 @@ import /* waitForState, */ {
 } from "../../utils/waitForPlayerState";
 import XHRMock from "../../utils/request_mock";
 
-describe("loadVideo Options", () => {
+describe.only("loadVideo Options", () => {
   let player;
   let xhrMock;
 
@@ -286,238 +286,143 @@ describe("loadVideo Options", () => {
     });
   });
 
-  describe("transportOptions", () => {
-    describe("representationFilter", () => {
-      it("should filter out Representations", async () => {
-        const videoRepresentations = manifestInfos
-          .periods[0].adaptations.video[0].representations;
-        const initialNumberOfRepresentations = videoRepresentations.length;
-        expect(initialNumberOfRepresentations).to.be.above(1);
-        const representationInTheMiddle = videoRepresentations[
-          Math.floor(initialNumberOfRepresentations / 2)
-        ];
+  describe("representationFilter", () => {
+    it("should filter out Representations", async () => {
+      const videoRepresentations = manifestInfos
+        .periods[0].adaptations.video[0].representations;
+      const initialNumberOfRepresentations = videoRepresentations.length;
+      expect(initialNumberOfRepresentations).to.be.above(1);
+      const representationInTheMiddle = videoRepresentations[
+        Math.floor(initialNumberOfRepresentations / 2)
+      ];
 
-        let numberOfTimeRepresentationFilterIsCalledForVideo = 0;
+      let numberOfTimeRepresentationFilterIsCalledForVideo = 0;
+      player.loadVideo({
+        transport: manifestInfos.transport,
+        url: manifestInfos.url,
+        representationFilter(representation, infos) {
+          if (infos.bufferType === "video") {
+            numberOfTimeRepresentationFilterIsCalledForVideo++;
+            return representation.bitrate <
+              representationInTheMiddle.bitrate;
+          }
+          return true;
+        },
+      });
+      await waitForLoadedStateAfterLoadVideo(player);
+
+      expect(numberOfTimeRepresentationFilterIsCalledForVideo)
+        .to.equal(initialNumberOfRepresentations);
+
+      const currentVideoTrack = player.getAvailableVideoTracks()
+        .find(track => track.active);
+
+      expect(currentVideoTrack.representations).to.have.length(
+        Math.floor(initialNumberOfRepresentations / 2)
+      );
+    });
+  });
+
+  describe("segmentLoader", () => {
+    let numberOfTimeCustomManifestLoaderWasCalled = 0;
+
+    beforeEach(() => {
+      numberOfTimeCustomManifestLoaderWasCalled = 0;
+    });
+
+    const customManifestLoader = ({ url }, callbacks) => {
+      numberOfTimeCustomManifestLoaderWasCalled++;
+      const xhr = new XMLHttpRequest();
+      const sendingTime = Date.now();
+
+      xhr.onload = (r) => {
+        if (200 <= xhr.status && xhr.status < 300) {
+          const duration = Date.now() - sendingTime;
+          const size = r.total;
+          const data = xhr.response;
+          callbacks.resolve({ duration, size, data });
+        } else {
+          const err = new Error("didn't work");
+          err.xhr = xhr;
+          callbacks.reject(err);
+        }
+      };
+
+      xhr.onerror = () => {
+        const err = new Error("didn't work");
+        err.xhr = xhr;
+        callbacks.reject(err);
+      };
+
+      xhr.open("GET", url);
+      xhr.responseType = "document";
+
+      xhr.send();
+
+      return () => {
+        xhr.abort();
+      };
+    };
+
+
+    it("should pass through the custom manifestLoader for manifest requests", async () => {
+      player.loadVideo({
+        transport: manifestInfos.transport,
+        url: manifestInfos.url,
+        manifestLoader: customManifestLoader,
+      });
+      await waitForLoadedStateAfterLoadVideo(player);
+
+      expect(numberOfTimeCustomManifestLoaderWasCalled)
+        .to.equal(1);
+    });
+
+    it("should pass through the custom segmentLoader even when no hint is given about the URL", () => {
+      const fakeMpdWithoutBaseURLs = `<?xml version="1.0" encoding="utf-8"?>
+<MPD xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns="urn:mpeg:dash:schema:mpd:2011"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+xsi:schemaLocation="urn:mpeg:DASH:schema:MPD:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd"
+profiles="urn:mpeg:dash:profile:isoff-live:2011"
+type="dynamic"
+minimumUpdatePeriod="PT500S"
+suggestedPresentationDelay="PT1S"
+availabilityStartTime="2022-12-07T08:52:13.150Z"
+publishTime="2022-12-07T08:52:13.926Z"
+maxSegmentDuration="PT1.0S"
+minBufferTime="PT2.0S">
+<Period id="0" start="PT0.0S">
+  <AdaptationSet id="0" contentType="video" startWithSAP="1" segmentAlignment="true" bitstreamSwitching="true" frameRate="25/1" maxWidth="768" maxHeight="576" par="4:3">
+    <Representation id="0" mimeType="video/mp4" codecs="avc1.640028" bandwidth="176736" width="768" height="576" sar="1:1">
+      <SegmentTemplate timescale="1000000" duration="1000000" initialization="init-stream$RepresentationID$.m4s" media="chunk-stream$RepresentationID$-$Number%05d$.m4s" startNumber="1">
+      </SegmentTemplate>
+    </Representation>
+  </AdaptationSet>
+  <AdaptationSet id="1" contentType="audio" startWithSAP="1" segmentAlignment="true" bitstreamSwitching="true">
+    <Representation id="1" mimeType="audio/mp4" codecs="mp4a.40.2" bandwidth="69000" audioSamplingRate="44100">
+      <AudioChannelConfiguration schemeIdUri="urn:mpeg:dash:23003:3:audio_channel_configuration:2011" value="1" />
+      <SegmentTemplate timescale="1000000" duration="1000000" initialization="init-stream$RepresentationID$.m4s" media="chunk-stream$RepresentationID$-$Number%05d$.m4s" startNumber="1">
+      </SegmentTemplate>
+    </Representation>
+  </AdaptationSet>
+</Period>
+</MPD>`;
+      return new Promise((res, rej) => {
         player.loadVideo({
           transport: manifestInfos.transport,
-          url: manifestInfos.url,
-          transportOptions: {
-            representationFilter(representation, infos) {
-              if (infos.bufferType === "video") {
-                numberOfTimeRepresentationFilterIsCalledForVideo++;
-                return representation.bitrate <
-                  representationInTheMiddle.bitrate;
-              }
-              return true;
-            },
+          manifestLoader(_url, callbacks) {
+            callbacks.resolve({ data: fakeMpdWithoutBaseURLs });
+          },
+          segmentLoader(infos) {
+            expect(infos.url).to.satisfy((s) => s.includes("init-stream") ||
+                                                s.includes("chunk-stream"));
+            player.stop();
+            res();
           },
         });
-        await waitForLoadedStateAfterLoadVideo(player);
-
-        expect(numberOfTimeRepresentationFilterIsCalledForVideo)
-          .to.equal(initialNumberOfRepresentations);
-
-        const currentVideoTrack = player.getAvailableVideoTracks()
-          .find(track => track.active);
-
-        expect(currentVideoTrack.representations).to.have.length(
-          Math.floor(initialNumberOfRepresentations / 2)
-        );
-      });
-    });
-
-    describe("segmentLoader", () => {
-      let numberOfTimeCustomSegmentLoaderWasCalled = 0;
-      let numberOfTimeCustomSegmentLoaderWentThrough = 0;
-
-      beforeEach(() => {
-        numberOfTimeCustomSegmentLoaderWasCalled = 0;
-        numberOfTimeCustomSegmentLoaderWentThrough = 0;
-      });
-
-      const customSegmentLoader = (info, callbacks) => {
-        numberOfTimeCustomSegmentLoaderWasCalled++;
-
-        // we will only use this custom loader for videos segments.
-        if (info.type !== "video") {
-          callbacks.fallback();
-          return;
-        }
-
-        numberOfTimeCustomSegmentLoaderWentThrough++;
-        const xhr = new XMLHttpRequest();
-        const sendingTime = Date.now();
-
-        xhr.onload = (r) => {
-          if (200 <= xhr.status && xhr.status < 300) {
-            const duration = Date.now() - sendingTime;
-            const size = r.total;
-            const data = xhr.response;
-            callbacks.resolve({ duration, size, data });
-          } else {
-            const err = new Error("didn't work");
-            err.xhr = xhr;
-            callbacks.reject(err);
-          }
-        };
-
-        xhr.onerror = () => {
-          const err = new Error("didn't work");
-          err.xhr = xhr;
-          callbacks.reject(err);
-        };
-
-        xhr.open("GET", info.url);
-        xhr.responseType = "arraybuffer";
-
-        const range = info.range;
-        if (range) {
-          if (range[1] && range[1] !== Infinity) {
-            xhr.setRequestHeader("Range", `bytes=${range[0]}-${range[1]}`);
-          } else {
-            xhr.setRequestHeader("Range", `bytes=${range[0]}-`);
-          }
-        }
-
-        xhr.send();
-
-        return () => {
-          xhr.abort();
-        };
-      };
-
-      it("should pass through the custom segmentLoader for segment requests", async () => {
-        xhrMock.lock();
-        let nbVideoSegmentRequests = 0;
-        player.loadVideo({
-          transport: manifestInfos.transport,
-          url: manifestInfos.url,
-          transportOptions: { segmentLoader: customSegmentLoader },
+        player.addEventListener("error", (err) => {
+          rej(err);
         });
-
-        await sleep(1);
-        await xhrMock.flush(); // Manifest request
-        await sleep(1);
-        expect(numberOfTimeCustomSegmentLoaderWasCalled)
-          .to.equal(4); // init + media Segment requests
-        nbVideoSegmentRequests += xhrMock.getLockedXHR()
-          .filter(r => r.url && r.url.includes("ateam-video"))
-          .length;
-        await xhrMock.flush();
-        await sleep(1);
-        expect(numberOfTimeCustomSegmentLoaderWasCalled)
-          .to.equal(6); // Segment requests
-        nbVideoSegmentRequests += xhrMock.getLockedXHR()
-          .filter(r => r.url && r.url.includes("ateam-video"))
-          .length;
-        expect(numberOfTimeCustomSegmentLoaderWentThrough)
-          .to.equal(nbVideoSegmentRequests);
-        expect(nbVideoSegmentRequests).to.be.above(0);
-      });
-
-      it("should pass through the custom segmentLoader even when no hint is given about the URL", () => {
-        const fakeMpdWithoutBaseURLs = `<?xml version="1.0" encoding="utf-8"?>
-<MPD xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xmlns="urn:mpeg:dash:schema:mpd:2011"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  xsi:schemaLocation="urn:mpeg:DASH:schema:MPD:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd"
-  profiles="urn:mpeg:dash:profile:isoff-live:2011"
-  type="dynamic"
-  minimumUpdatePeriod="PT500S"
-  suggestedPresentationDelay="PT1S"
-  availabilityStartTime="2022-12-07T08:52:13.150Z"
-  publishTime="2022-12-07T08:52:13.926Z"
-  maxSegmentDuration="PT1.0S"
-  minBufferTime="PT2.0S">
-  <Period id="0" start="PT0.0S">
-    <AdaptationSet id="0" contentType="video" startWithSAP="1" segmentAlignment="true" bitstreamSwitching="true" frameRate="25/1" maxWidth="768" maxHeight="576" par="4:3">
-      <Representation id="0" mimeType="video/mp4" codecs="avc1.640028" bandwidth="176736" width="768" height="576" sar="1:1">
-        <SegmentTemplate timescale="1000000" duration="1000000" initialization="init-stream$RepresentationID$.m4s" media="chunk-stream$RepresentationID$-$Number%05d$.m4s" startNumber="1">
-        </SegmentTemplate>
-      </Representation>
-    </AdaptationSet>
-    <AdaptationSet id="1" contentType="audio" startWithSAP="1" segmentAlignment="true" bitstreamSwitching="true">
-      <Representation id="1" mimeType="audio/mp4" codecs="mp4a.40.2" bandwidth="69000" audioSamplingRate="44100">
-        <AudioChannelConfiguration schemeIdUri="urn:mpeg:dash:23003:3:audio_channel_configuration:2011" value="1" />
-        <SegmentTemplate timescale="1000000" duration="1000000" initialization="init-stream$RepresentationID$.m4s" media="chunk-stream$RepresentationID$-$Number%05d$.m4s" startNumber="1">
-        </SegmentTemplate>
-      </Representation>
-    </AdaptationSet>
-  </Period>
-</MPD>`;
-        return new Promise((res, rej) => {
-          player.loadVideo({
-            transport: manifestInfos.transport,
-            transportOptions: {
-              manifestLoader(_url, callbacks) {
-                callbacks.resolve({ data: fakeMpdWithoutBaseURLs });
-              },
-              segmentLoader(infos) {
-                expect(infos.url).to.satisfy((s) => s.includes("init-stream") ||
-                                                    s.includes("chunk-stream"));
-                player.stop();
-                res();
-              },
-            },
-          });
-          player.addEventListener("error", (err) => {
-            rej(err);
-          });
-        });
-      });
-    });
-
-    describe("manifestLoader", () => {
-      let numberOfTimeCustomManifestLoaderWasCalled = 0;
-
-      beforeEach(() => {
-        numberOfTimeCustomManifestLoaderWasCalled = 0;
-      });
-
-      const customManifestLoader = ({ url }, callbacks) => {
-        numberOfTimeCustomManifestLoaderWasCalled++;
-        const xhr = new XMLHttpRequest();
-        const sendingTime = Date.now();
-
-        xhr.onload = (r) => {
-          if (200 <= xhr.status && xhr.status < 300) {
-            const duration = Date.now() - sendingTime;
-            const size = r.total;
-            const data = xhr.response;
-            callbacks.resolve({ duration, size, data });
-          } else {
-            const err = new Error("didn't work");
-            err.xhr = xhr;
-            callbacks.reject(err);
-          }
-        };
-
-        xhr.onerror = () => {
-          const err = new Error("didn't work");
-          err.xhr = xhr;
-          callbacks.reject(err);
-        };
-
-        xhr.open("GET", url);
-        xhr.responseType = "document";
-
-        xhr.send();
-
-        return () => {
-          xhr.abort();
-        };
-      };
-
-      it("should pass through the custom manifestLoader for manifest requests", async () => {
-        player.loadVideo({
-          transport: manifestInfos.transport,
-          url: manifestInfos.url,
-          transportOptions: { manifestLoader: customManifestLoader },
-        });
-        await waitForLoadedStateAfterLoadVideo(player);
-
-        expect(numberOfTimeCustomManifestLoaderWasCalled)
-          .to.equal(1);
       });
     });
   });
