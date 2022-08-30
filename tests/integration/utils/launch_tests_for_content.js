b78cf2394328c57d1f1ffa0c43171e5a5db76819
Paul Berberian <pea.berberian@gmail.com>
2022-08-30 15:43:15 +0200

    Replace getUrl method by getContentUrls

--- tests/integration/utils/launch_tests_for_content.js
+++ tests/integration/utils/launch_tests_for_content.js
@@ -621,7 +621,7 @@ export default function launchTestsForContent(manifestInfos) {
       }
     });
 
-    describe("getUrl", () => {
+    describe("getContentUrls", () => {
       it("should return the URL of the manifest", async () => {
         player.loadVideo({
           url: manifestInfos.url,
@@ -629,7 +629,7 @@ export default function launchTestsForContent(manifestInfos) {
           autoPlay: false,
         });
         await waitForLoadedStateAfterLoadVideo(player);
-        expect(player.getUrl()).to.eql(manifestInfos.url);
+        expect(player.getContentUrls()).to.eql([manifestInfos.url]);
       });
     });
 
