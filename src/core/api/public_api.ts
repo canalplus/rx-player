d3a363c167c4ea685b888360986845c125deed70
Paul Berberian <pea.berberian@gmail.com>
2022-10-04 15:44:28 +0200

    [v4] Remove decipherabilityUpdaate event
    This is a proposal for removing the decipherabilityUpdaate event from
    the future v4, which was an event indicating that some
    Representation(s) became un-decipherable or decipherable, for the
    following reasons:
    
      - It exposed some properties we may still want to make evolutions to.
        The main example is the `decipherable` property, typed as a
        `boolean | undefined`, where it could make sense in the future to
        set to a fourth-"unknown" value (which would be different to
        the "not handled yet" case - thus just setting both to `undefined`
        would not be sufficient).
    
        This fourth case has for now only be encountered in our
        key-expiration-handling work.
    
      - As far as we know, this event has never been handled. Its original
        point was to let applications listen to it so they can filter out
        the corresponding qualities for subsequent playback of similar
        contents.
    
        But, as we know, applications usually take another (usually better)
        route - when they do: They mostly poll capabilities before playback,
        hence knowing even before the first loaded content what quality may
        be playable.
    
        Here also handling decipherability updates at play time may not be
        as straightforward, as you may have to also account for the current
        quality being refused, whether it was for HDCP, CDM or perhaps just
        a temporary license server error etc.
    
        To be clear, the event removed DO NOT remove the RxPlayer's ability
        to fallback during playback, it just removes the main event
        signalling to the application that a `Representation`'s decryption
        key is not usable.
    
      - The most useful indication of this event, that some quality was
        fallbacked from, can be mostly replicated by listening to the
        corresponding `warning` event sending an error with a
        `KEY_STATUSES_CHANGE_ERROR` code.
        The corresponding key status AND key id should become part of that
        error very soon.

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -55,7 +55,6 @@ import {
   IAvailableVideoTrack,
   IBrokenRepresentationsLockContext,
   IConstructorOptions,
-  IDecipherabilityUpdateContent,
   IKeySystemConfigurationOutput,
   IKeySystemOption,
   ILoadVideoOptions,
@@ -759,16 +758,6 @@ class Player extends EventEmitter<IPublicAPIEvent> {
       this.trigger("streamEvent", streamEvent));
     initializer.addEventListener("streamEventSkip", (streamEventSkip) =>
       this.trigger("streamEventSkip", streamEventSkip));
-    initializer.addEventListener("decipherabilityUpdate", (decipherabilityUpdate) =>
-      this.trigger("decipherabilityUpdate", decipherabilityUpdate.map((i) => {
-        return { periodInfo: { start: i.period.start,
-                               end: i.period.end,
-                               id: i.period.id },
-                 trackType: i.adaptation.type,
-                 trackId: i.adaptation.id,
-                 representationId: i.representation.id,
-                 isDecipherable: i.representation.decipherable };
-      })));
     initializer.addEventListener("activePeriodChanged", (periodInfo) =>
       this._priv_onActivePeriodChanged(contentInfos, periodInfo));
     initializer.addEventListener("periodStreamReady", (periodReadyInfo) =>
@@ -2718,7 +2707,6 @@ interface IPublicAPIEvent {
   availableAudioTracksChange : IAvailableAudioTrack[];
   availableTextTracksChange : IAvailableTextTrack[];
   availableVideoTracksChange : IAvailableVideoTrack[];
-  decipherabilityUpdate : IDecipherabilityUpdateContent[];
   play: null;
   pause: null;
   newAvailablePeriods : IPeriod[];
