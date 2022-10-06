d77e3dfa884d12633b1c0398c4778e4cfb9d2e57
Paul Berberian <pea.berberian@gmail.com>
2022-10-06 16:29:49 +0200

    Rename the networkConfig loadVideo option into a requestConfig option

--- src/core/api/option_utils.ts
+++ src/core/api/option_utils.ts
@@ -28,7 +28,7 @@ import {
   ILoadedManifestFormat,
   ILoadVideoOptions,
   IManifestLoader,
-  INetworkConfigOption,
+  IRequestConfig,
   IRepresentationFilter,
   ISegmentLoader,
   IServerSyncInfos,
@@ -70,7 +70,7 @@ interface IParsedLoadVideoOptionsBase {
   keySystems : IKeySystemOption[];
   lowLatencyMode : boolean;
   minimumManifestUpdateInterval : number;
-  networkConfig: INetworkConfigOption;
+  requestConfig: IRequestConfig;
   startAt : IParsedStartAtOption|undefined;
   enableFastSwitching : boolean;
   defaultAudioTrackSwitchingMode : IAudioTrackSwitchingMode | undefined;
@@ -399,8 +399,7 @@ function parseLoadVideoOptions(
     }
   }
 
-  const networkConfig = options.networkConfig ?? {};
-
+  const requestConfig = options.requestConfig ?? {};
 
   // All those eslint disable are needed because the option is voluntarily
   // hidden from the base type to limit discovery of this hidden API.
@@ -421,7 +420,7 @@ function parseLoadVideoOptions(
            manifestLoader: options.manifestLoader,
            manifestUpdateUrl: options.manifestUpdateUrl,
            minimumManifestUpdateInterval,
-           networkConfig,
+           requestConfig,
            onCodecSwitch,
            referenceDateTime: options.referenceDateTime,
            representationFilter: options.representationFilter,
