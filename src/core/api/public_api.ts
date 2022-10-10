b90336092d49bc5f9e3cd9a8c36a2e7117bae06f
Paul Berberian <pea.berberian@gmail.com>
2022-10-10 18:38:13 +0200

    Replace autoTrackSwitch event by a more useful trackUpdate event
    Until now, the RxPlayer v4.0.0 was scheduled to introduce the
    `autoTrackSwitch` event to signal that the track of any type (audio,
    video, text...) and Period had to be changed automatically by the RxPlayer
    (for now the only - improbable - case is when the corresponding track
    just disappears after a Manifest update - e.g. the corresponding
    `AdaptationSet` element is removed from a DASH MPD).
    
    This event was needed to perfectly replace the track preferences and
    bitrate APIs - which are removed from the v4.0.0 for more powerful API -
    as the track and wanted Representations would without this
    event just silently be updated by the RxPlayer, without the application
    knowing about it and being able to react upon it.
    
    But after some tests, it seems that adding a much more general
    `trackUpdate` event, signaling any type of track change for any type and
    Period (that is, even when changed manually for example through a
    `setAudioTrack` call) is much more easy-to-understand and useful.
    And it isn't hard to implement.
    
    Because an application might still want to know the reason for that
    event (is it due to the track disappearing from the Manifest? It is
    linked to a `setAudioTrack` call?), the `reason` property of that event
    has been updated accordingly. It can now be set to:
      - `"manual"`: Explicit track modification, e.g. `setAudioTrack`
      - `"trickmode-enabled"`: Trickmode tracks have been enabled through
        `setPlaybackRate` (generally, this is linked to newly set video
        trickmode tracks)
      - `"trickmode-disabled"`: Trickmode tracks have been disabled through
        `setPlaybackRate` (generally, this is linked to newly unset video
        trickmode tracks).
      - `"missing"`: The track does not exist in the Manifest anymore
    
    We still reserve the right to add new, non-colliding, reasons in the
    future.

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -56,7 +56,7 @@ import {
   ILoadVideoOptions,
   ILockedAudioRepresentationsSettings,
   ILockedVideoRepresentationsSettings,
-  IAutoTrackSwitchEventPayload,
+  ITrackUpdateEventPayload,
   IPeriod,
   IPeriodChangeEvent,
   IPlayerError,
@@ -2022,8 +2022,8 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     contentInfos.tracksStore.addEventListener("brokenRepresentationsLock", (e) => {
       this.trigger("brokenRepresentationsLock", e);
     });
-    contentInfos.tracksStore.addEventListener("autoTrackSwitch", (e) => {
-      this.trigger("autoTrackSwitch", e);
+    contentInfos.tracksStore.addEventListener("trackUpdate", (e) => {
+      this.trigger("trackUpdate", e);
     });
 
     contentInfos.tracksStore.updatePeriodList(manifest);
@@ -2554,7 +2554,7 @@ interface IPublicAPIEvent {
   availableVideoTracksChange : IAvailableVideoTrack[];
   newAvailablePeriods : IPeriod[];
   brokenRepresentationsLock : IBrokenRepresentationsLockContext;
-  autoTrackSwitch : IAutoTrackSwitchEventPayload;
+  trackUpdate : ITrackUpdateEventPayload;
   seeking : null;
   seeked : null;
   streamEvent : IStreamEvent;
