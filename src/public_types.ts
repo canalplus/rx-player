6cd05664444067bd7fa1beca67c1502271ee0236
Paul Berberian <pea.berberian@gmail.com>
2022-10-12 15:11:49 +0200

    EME robustnesses can also be undefined

--- src/public_types.ts
+++ src/public_types.ts
@@ -552,7 +552,7 @@ export type IAudioCapabilitiesConfiguration = IRobustnessMediaKeySystemCapabilit
  */
 export interface IRobustnessMediaKeySystemCapabilities {
   type: "robustness";
-  value : string[];
+  value : Array<string | undefined>;
 }
 
 /**
