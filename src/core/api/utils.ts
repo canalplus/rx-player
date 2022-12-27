0bca40683e4edbff3df82c3b248d46bd550ba1b0
Paul Berberian <pea.berberian@gmail.com>
2022-12-27 16:37:56 +0100

    Fix post-RELOADING state update

--- src/core/api/utils.ts
+++ src/core/api/utils.ts
@@ -127,6 +127,8 @@ export function constructPlayerStateReference(
           playerStateRef.setValue(newState);
         }
       }
+    } else if (playerStateRef.getValue() === PLAYER_STATES.RELOADING) {
+      playerStateRef.setValue(getLoadedContentState(mediaElement, null));
     } else {
       updateStateIfLoaded(null);
     }
