e6541233d3aa286521532e73469d8773f86094ee
Paul Berberian <pea.berberian@gmail.com>
2023-01-20 11:06:31 +0100

    api: rename various types into trackType

--- src/public_types.ts
+++ src/public_types.ts
@@ -380,8 +380,7 @@ export interface ISegmentLoaderContext {
    */
   byteRanges? : Array<[number, number]> | undefined;
   /** Type of the corresponding track. */
-  // XXX TODO trackType instead?
-  type : ITrackType;
+  trackType : ITrackType;
 }
 
 /** Every possible value for the Adaptation's `type` property. */
@@ -679,7 +678,7 @@ export type IPlayerError = EncryptedMediaError |
  * Information describing a single Representation from an Adaptation, to be used
  * in the `representationFilter` API.
  */
-export interface IRepresentationContext { bufferType: string;
+export interface IRepresentationContext { trackType: string;
                                           language?: string | undefined;
                                           isAudioDescription? : boolean | undefined;
                                           isClosedCaption? : boolean | undefined;
