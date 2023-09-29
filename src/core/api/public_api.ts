fe4379a22db26e477b279a40829f34ab360b099f
Paul Berberian <paul.berberian+github@proton.me>
2023-09-29 20:05:12 +0200

    Update builds and version

--- src/core/api/public_api.ts
+++ src/core/api/public_api.ts
@@ -346,7 +346,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1194624
     videoElement.preload = "auto";
 
-    this.version = /* PLAYER_VERSION */"3.32.1";
+    this.version = /* PLAYER_VERSION */"4.0.0-beta.3";
     this.log = log;
     this.state = "STOPPED";
     this.videoElement = videoElement;
@@ -2809,7 +2809,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     }
   }
 }
-Player.version = /* PLAYER_VERSION */"3.32.1";
+Player.version = /* PLAYER_VERSION */"4.0.0-beta.3";
 
 /** Every events sent by the RxPlayer's public API. */
 interface IPublicAPIEvent {
