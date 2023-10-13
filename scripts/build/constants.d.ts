5fe7ec1d86f7c974544767ce7b5b547a1c07c82a
Paul Berberian <paul.berberian+github@proton.me>
2023-10-13 12:16:21 +0200

    Default RxPlayer import now also depends on modular builds
    This commmit performs several actions to simplify the building process
    of the RxPlayer:
    
      - The default RxPlayer build, imported e.g. through the `import RxPlayer
        from "rx-player"` path, now rely on the same non-bundled path than
        other such imports.
    
        Previously, it relied on our bundled file - which complexified our
        logic and might prevent applications from doing the same
        optimizations they could do on non-pre-bundled dependencies.
    
        Now the bundled files still exist, but will only be communicated
        through release notes (the point of the bundled files is just to
        have fast-to-test files that can directly be inserted in a
        `<script>` tag).
    
      - We do not rely on bundling-time constants to announce which feature
        should be added into which build anymore.
        The RxPlayer code now just add its own features depending on the
        choosen build (e.g. the default build which imports `src/index.ts`
        now imports explicitely the wanted features) by relying on the
        features API, like any external code would.
    
        This leads to a lot of code simplification, it also removes the
        need to have both commonJS and ES6 style imports in the code which
        was a huge pain when interacting with tools.
    
        We're also dogfooding our own feature switching API in our own code,
        which should hopefully be better to detect any issue with it.

--- scripts/build/constants.d.ts
+++ scripts/build/constants.d.ts
@@ -16,7 +16,7 @@
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
-// Define build-time constants corresponding to the modular build.
+// Define build-time constants corresponding to the regular build.
 
 declare const enum __LOGGER_LEVEL__ {
   CURRENT_LEVEL = "NONE",
@@ -26,28 +26,6 @@ declare const enum __ENVIRONMENT__ {
   PRODUCTION = 0,
   DEV = 1,
   CURRENT_ENV = PRODUCTION,
-}
-
-declare const enum __FEATURES__ {
-  IS_DISABLED = 0,
-  IS_ENABLED = 1,
-
-  BIF_PARSER = IS_DISABLED,
-  DASH = IS_DISABLED,
-  DIRECTFILE = IS_DISABLED,
-  EME = IS_DISABLED,
-  HTML_SAMI = IS_DISABLED,
-  HTML_SRT = IS_DISABLED,
-  HTML_TTML = IS_DISABLED,
-  HTML_VTT = IS_DISABLED,
-  LOCAL_MANIFEST = IS_DISABLED,
-  METAPLAYLIST = IS_DISABLED,
-  DEBUG_ELEMENT = IS_DISABLED,
-  NATIVE_SAMI = IS_DISABLED,
-  NATIVE_SRT = IS_DISABLED,
-  NATIVE_TTML = IS_DISABLED,
-  NATIVE_VTT = IS_DISABLED,
-  SMOOTH = IS_DISABLED,
 }
 
 declare const __RX_PLAYER_DEBUG_MODE__ : boolean | undefined;
