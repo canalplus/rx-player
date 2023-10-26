3d05a9182a736e0b88c4eabbf65b83434fe75ff3
Paul Berberian <paul.berberian+github@proton.me>
2023-10-26 13:51:30 +0200

    Remove now removed image-related features from tests

--- src/features/list/__tests__/index.test.ts
+++ src/features/list/__tests__/index.test.ts
@@ -1,4 +1,3 @@
-import { BIF_PARSER as BIF_PARSER_FEATURE } from "../bif_parser";
 import { DASH as DASH_FEATURE } from "../dash";
 import { DIRECTFILE as DIRECTFILE_FEATURE } from "../directfile";
 import { EME as EME_FEATURE } from "../eme";
@@ -7,9 +6,7 @@ import { HTML_SRT_PARSER as HTML_SRT_PARSER_FEATURE } from "../html_srt_parser";
 import { HTML_TEXT_BUFFER as  HTML_TEXT_BUFFER_FEATURE } from "../html_text_buffer";
 import { HTML_TTML_PARSER as  HTML_TTML_PARSER_FEATURE } from "../html_ttml_parser";
 import { HTML_VTT_PARSER as HTML_VTT_PARSER_FEATURE } from "../html_vtt_parser";
-import { IMAGE_BUFFER as IMAGE_BUFFER_FEATURE } from "../image_buffer";
 import {
-  BIF_PARSER,
   DASH,
   DIRECTFILE,
   EME,
@@ -18,7 +15,6 @@ import {
   HTML_TEXT_BUFFER,
   HTML_TTML_PARSER,
   HTML_VTT_PARSER,
-  IMAGE_BUFFER,
   NATIVE_SAMI_PARSER,
   NATIVE_SRT_PARSER,
   NATIVE_TEXT_BUFFER,
@@ -35,7 +31,6 @@ import { SMOOTH as SMOOTH_FEATURE } from "../smooth";
 
 describe("Features list", () => {
   it("should export all stable features", () => {
-    expect(BIF_PARSER).toBe(BIF_PARSER_FEATURE);
     expect(DASH).toBe(DASH_FEATURE);
     expect(DIRECTFILE).toBe(DIRECTFILE_FEATURE);
     expect(EME).toBe(EME_FEATURE);
@@ -44,7 +39,6 @@ describe("Features list", () => {
     expect(HTML_TEXT_BUFFER).toBe(HTML_TEXT_BUFFER_FEATURE);
     expect(HTML_TTML_PARSER).toBe(HTML_TTML_PARSER_FEATURE);
     expect(HTML_VTT_PARSER).toBe(HTML_VTT_PARSER_FEATURE);
-    expect(IMAGE_BUFFER).toBe(IMAGE_BUFFER_FEATURE);
     expect(NATIVE_SAMI_PARSER).toBe(NATIVE_SAMI_PARSER_FEATURE);
     expect(NATIVE_SRT_PARSER).toBe(NATIVE_SRT_PARSER_FEATURE);
     expect(NATIVE_TEXT_BUFFER).toBe(NATIVE_TEXT_BUFFER_FEATURE);
