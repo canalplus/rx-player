92a1fed9228e21487f99c5bc6074c041e356aed1
Paul Berberian <paul.berberian+github@proton.me>
2023-12-21 13:33:31 +0100

    DEBUG_ELEMENT is not an experimental feature anymore

--- src/experimental/features/__tests__/index.test.ts
+++ src/experimental/features/__tests__/index.test.ts
@@ -1,6 +1,4 @@
-import { DEBUG_ELEMENT as FEATURE_DEBUG_ELEMENT } from "../debug_element";
 import {
-  DEBUG_ELEMENT,
   METAPLAYLIST,
   LOCAL_MANIFEST,
 } from "../index";
@@ -9,7 +7,6 @@ import { METAPLAYLIST as FEATURE_METAPLAYLIST } from "../metaplaylist";
 
 describe("Experimental Features", () => {
   it("should export all experimental features", () => {
-    expect(DEBUG_ELEMENT).toBe(FEATURE_DEBUG_ELEMENT);
     expect(METAPLAYLIST).toBe(FEATURE_METAPLAYLIST);
     expect(LOCAL_MANIFEST).toBe(FEATURE_LOCAL_MANIFEST);
   });
