4f99eca5f6de4938e5720d70fa0b634664aeb5d5
Paul Berberian <paul.berberian+github@proton.me>
2024-01-26 12:41:06 +0100

    Move getBufferedDataPerMediaBuffer into src/core/main/utils

---
+++ src/core/main/utils/get_buffered_data_per_media_buffer.ts
@@ -0,0 +1,46 @@
+import type { ITextDisplayer } from "../../../main_thread/types";
+import type { IMediaSourceInterface } from "../../../mse";
+import { SourceBufferType } from "../../../mse";
+import type { ITrackType } from "../../../public_types";
+import arrayFind from "../../../utils/array_find";
+import type { IRange } from "../../../utils/ranges";
+
+/**
+ * Returns a JS object where keys are the type of buffers (e.g. "audio",
+ * "video", "text") and values are the corresponding range of buffered
+ * data according to the given `IMediaSourceInterface` (or `null` if not
+ * known / nothing is buffered).
+ * @param {Object|null} mediaSourceInterface
+ * @param {Object|null} textDisplayer
+ * @returns {Object}
+ */
+export default function getBufferedDataPerMediaBuffer(
+  mediaSourceInterface: IMediaSourceInterface | null,
+  textDisplayer: ITextDisplayer | null
+): Record<ITrackType, IRange[] | null> {
+  const buffered: Record<ITrackType, IRange[] | null> = {
+    audio: null,
+    video: null,
+    text: null,
+  };
+  if (textDisplayer !== null) {
+    buffered.text = textDisplayer.getBufferedRanges();
+  }
+  if (mediaSourceInterface === null) {
+    return buffered;
+  }
+  const audioBuffer = arrayFind(mediaSourceInterface.sourceBuffers,
+                                s => s.type === SourceBufferType.Audio);
+  const videoBuffer = arrayFind(mediaSourceInterface.sourceBuffers,
+                                s => s.type === SourceBufferType.Video);
+  const audioBuffered = audioBuffer?.getBuffered();
+  if (audioBuffered !== undefined) {
+    buffered.audio = audioBuffered;
+  }
+  const videoBuffered = videoBuffer?.getBuffered();
+  if (videoBuffered !== undefined) {
+    buffered.video = videoBuffered;
+  }
+  return buffered;
+}
+
