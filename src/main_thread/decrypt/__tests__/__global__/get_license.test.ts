e4d168034f264fa8fcb591f5ee376408902367af
Florent <florent.bouisset@gmail.com>
2024-02-01 15:15:57 +0100

    refactor: create a fn getRetryDelay and test it
    maxDelay now restrict the value of the delay after that the fuzzFactor
    has been applied. This ensure that the returned delay is trully
    equal or inferior the the maxDelay.
    This would reduce from 3900ms to 3000ms the maxDefault based on our
    current config but updating the value does not seem necessary.

--- src/main_thread/decrypt/__tests__/__global__/get_license.test.ts
+++ src/main_thread/decrypt/__tests__/__global__/get_license.test.ts
@@ -25,6 +25,7 @@
 
 import type { IPlayerError } from "../../../../public_types";
 import { concat } from "../../../../utils/byte_parsing";
+import { getRetryDelay } from "../../../../utils/retry_promise_with_backoff";
 import {
   formatFakeChallengeFromInitData,
   MediaKeySessionImpl,
@@ -40,6 +41,37 @@ describe("decrypt - global tests - getLicense", () => {
     jest.resetModules();
     jest.restoreAllMocks();
   });
+
+  it("should retry after an increased time at each retry", () => {
+    /*
+    * The delay after each retry is multiplied by a factor 2.
+    * This delay is slightly modified to randomize the distribution.
+    * Following table display the expected delay for each retry, in the
+    * most pessimists and most optimistics scenarios.
+    * The last two columns show the minimum and maximum accumulated time
+    * waited since the first try.
+    *
+    * |Retry| base  |  max  |  min  |totalMax|totalMin |
+    * |-----|-------|-------|-------|--------|---------|
+    * | 1   |200    | 260   | 140   | 260    |  140    |
+    * | 2   |400    | 520   | 280   | 780    |  420    |
+    * | 3   |800    | 1040  | 560   | 1820   |  980    |
+    * | 4   |1600   | 2080  | 1120  | 3900   |  21OO   |
+    * | 5   |3000   | 3000  | 2100  | 7800   |  4200   |
+    * | 6   |3000   | 3000  | 2100  | 11700  |  6300   |
+    * | 7   |3000   | 3000  | 2100  | 15600  |  8400   |
+    */
+    const baseDelay = 200;
+    const maxDelay = 3000;
+    expect(getRetryDelay(baseDelay, 1, maxDelay)).toBeGreaterThanOrEqual(140);
+    expect(getRetryDelay(baseDelay, 1, maxDelay)).toBeLessThanOrEqual(260);
+
+    expect(getRetryDelay(baseDelay, 3, maxDelay)).toBeGreaterThanOrEqual(560);
+    expect(getRetryDelay(baseDelay, 3, maxDelay)).toBeLessThanOrEqual(1040);
+
+    expect(getRetryDelay(baseDelay, 6, maxDelay)).toBeGreaterThanOrEqual(2100);
+    expect(getRetryDelay(baseDelay, 6, maxDelay)).toBeLessThanOrEqual(3000);
+  })
 
   it("should update the session after getLicense resolves with a license", async () => {
     await checkGetLicense({
