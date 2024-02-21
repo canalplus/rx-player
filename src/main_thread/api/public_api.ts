9c906342df10fd9e97fdf19118cf3c8415352b92
Florent <florent.bouisset@gmail.com>
2024-02-21 14:24:15 +0100

    update version

--- src/main_thread/api/public_api.ts
+++ src/main_thread/api/public_api.ts
@@ -347,7 +347,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1194624
     videoElement.preload = "auto";
 
-    this.version = /* PLAYER_VERSION */ "4.0.0-rc.2";
+    this.version = /* PLAYER_VERSION */ "4.0.0-dev.2024022100";
     this.log = log;
     this.state = "STOPPED";
     this.videoElement = videoElement;
@@ -3158,7 +3158,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     }
   }
 }
-Player.version = /* PLAYER_VERSION */ "4.0.0-rc.2";
+Player.version = /* PLAYER_VERSION */ "4.0.0-dev.2024022100";
 
 /** Every events sent by the RxPlayer's public API. */
 interface IPublicAPIEvent {
