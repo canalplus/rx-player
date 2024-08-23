d51b3389653c1ec763bea141107a324130d4ade0
Paul Berberian <paul.berberian+github@proton.me>
2024-08-23 12:28:50 +0200

    DRM: Re-create MediaKeys for each content on Philips' NETTV
    Fixes #1464
    
    There's a new device (after LG and Panasonic TVs) which seems to have
    issues when relying on the same `MediaKeys` instance on multiple
    contents, which seems to be Philips's NETTV (both 2023 and 2024 devices
    seem to have the issue as far as we know, and they advertise completely
    different operating systems, so instead we identified the culprit as
    "NETTV", which seems to be the web browser application Philips developed
    for their TVs and which is present on web applications' user agent on
    those devices).
    
    So I disable the possibility for them here.
    I still have to check if it works because I took as user-agent something
    I found a little randomly on the web.
    
    Note that disabling re-usage of the `MediaKeys` instance will mean no
    in-memory `MediaKeySession` caching, which is a trick we use to both
    speed up loading a previous content and preventing to much requests
    on the license server also when doing that.
    
    But we know most other players don't seem to do such tricks and the
    speed up is not that much noticeable, so that shouldn't be a big issue.

--- src/compat/can_reuse_media_keys.ts
+++ src/compat/can_reuse_media_keys.ts
@@ -1,4 +1,4 @@
-import { isPanasonic, isWebOs } from "./browser_detection";
+import { isPanasonic, isPhilipsNetTv, isWebOs } from "./browser_detection";
 
 /**
  * Returns `true` if a `MediaKeys` instance (the  `Encrypted Media Extension`
@@ -9,9 +9,11 @@ import { isPanasonic, isWebOs } from "./browser_detection";
  *   - (2022-11-21): WebOS (LG TVs), for some encrypted contents, just
  *     rebuffered indefinitely when loading a content already-loaded on the
  *     HTMLMediaElement.
+ *   - (2024-08-23): Seen on Philips 2024 and 2023 in:
+ *     https://github.com/canalplus/rx-player/issues/1464
  *
  * @returns {boolean}
  */
 export default function canReuseMediaKeys(): boolean {
-  return !isWebOs && !isPanasonic;
+  return !isWebOs && !isPhilipsNetTv && !isPanasonic;
 }
