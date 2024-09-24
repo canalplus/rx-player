46c28d5ed9ae5dd72a47a4be19d4abc95e03e0b1
Paul Berberian <paul.berberian+github@proton.me>
2024-09-24 17:48:44 +0200

    wipwoop

--- src/main_thread/init/multi_thread_content_initializer.ts
+++ src/main_thread/init/multi_thread_content_initializer.ts
@@ -1103,9 +1103,15 @@ export default class MultiThreadContentInitializer extends ContentInitializer {
       }
     };
 
+    const onmessageerror = (_msg: MessageEvent) => {
+      log.error("MTCI: Error when receiving message from worker.");
+    };
+
     this._settings.worker.addEventListener("message", onmessage);
+    this._settings.worker.addEventListener("messageerror", onmessageerror);
     this._initCanceller.signal.register(() => {
       this._settings.worker.removeEventListener("message", onmessage);
+      this._settings.worker.removeEventListener("messageerror", onmessageerror);
     });
   }
 
