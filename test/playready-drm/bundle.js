0b1ae440f4e137ac04bd43083a677a619c78942d
Florent Bouisset <florent.bouisset@gmail.com>
2024-11-21 11:50:12 +0100

    demo: deploy test/playready-drm demo to the gh-pages

--- test/playready-drm/bundle.js
+++ test/playready-drm/bundle.js
@@ -71646,6 +71646,10 @@ ${event}`
       const challenge = isPlayready ? formatPlayreadyChallenge(rawChallenge) : rawChallenge;
       const xhr = new XMLHttpRequest();
       xhr.open("POST", licenseServerUrl, true);
+      xhr.setRequestHeader(
+        "X-Axdrm-Message",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjMzNjRlYjUtNTFmNi00YWUzLThjOTgtMzNjZWQ1ZTMxYzc4IiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsImtleXMiOlt7ImlkIjoiOWViNDA1MGQtZTQ0Yi00ODAyLTkzMmUtMjdkNzUwODNlMjY2IiwiZW5jcnlwdGVkX2tleSI6ImxLM09qSExZVzI0Y3Iya3RSNzRmbnc9PSJ9XX19.4lWwW46k-oWcah8oN18LPj5OLS5ZU-_AQv7fe0JhNjAeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjMzNjRlYjUtNTFmNi00YWUzLThjOTgtMzNjZWQ1ZTMxYzc4IiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsImtleXMiOlt7ImlkIjoiOWViNDA1MGQtZTQ0Yi00ODAyLTkzMmUtMjdkNzUwODNlMjY2IiwiZW5jcnlwdGVkX2tleSI6ImxLM09qSExZVzI0Y3Iya3RSNzRmbnc9PSJ9XX19.4lWwW46k-oWcah8oN18LPj5OLS5ZU-_AQv7fe0JhNjA"
+      );
       return new Promise((resolve, reject) => {
         xhr.onerror = () => {
           const error = new Error("getLicense's request failed on an error");
