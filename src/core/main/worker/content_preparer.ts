72d9efde5bf899fad6cfbaf9acfba2dbe2cabaad
Paul Berberian <paul.berberian@proton.me>
2025-01-21 10:46:11 +0100

    Fix remaining CoreInterface issues

--- src/core/main/worker/content_preparer.ts
+++ src/core/main/worker/content_preparer.ts
@@ -20,13 +20,13 @@ import type {
 import TaskCanceller from "../../../utils/task_canceller";
 import type { IRepresentationEstimator } from "../../adaptive";
 import createAdaptiveRepresentationSelector from "../../adaptive";
+import type { IRepresentationEstimatorThrottlers } from "../../adaptive/adaptive_representation_selector";
 import CmcdDataBuilder from "../../cmcd";
 import type { IManifestRefreshSettings } from "../../fetchers";
 import { ManifestFetcher, SegmentQueueCreator } from "../../fetchers";
 import SegmentSinksStore from "../../segment_sinks";
 import type { INeedsMediaSourceReloadPayload } from "../../stream";
 import DecipherabilityFreezeDetector from "../common/DecipherabilityFreezeDetector";
-import { limitVideoResolution, throttleVideoBitrate } from "./globals";
 import TrackChoiceSetter from "./track_choice_setter";
 import { formatErrorForSender } from "./utils";
 import WorkerTextDisplayerInterface from "./worker_text_displayer_interface";
@@ -75,6 +75,8 @@ export default class ContentPreparer {
   public initializeNewContent(
     sendMessage: (msg: IWorkerMessage, transferables?: Transferable[]) => void,
     context: IContentInitializationData,
+    /** Allows to filter which Representations can be choosen. */
+    throttlers: IRepresentationEstimatorThrottlers,
   ): Promise<IManifest> {
     return new Promise((res, rej) => {
       this.disposeCurrentContent();
@@ -89,9 +91,13 @@ export default class ContentPreparer {
 
       const transportFn = features.transports[transport];
       if (typeof transportFn !== "function") {
-        // Stop previous content and reset its state
-        // XXX TODO:  send fatal error
-        throw new Error(`transport "${transport}" not supported`);
+        rej(
+          new Error(
+            `transport "${transport}" not supported. ` +
+              "Did you add the corresponding feature?",
+          ),
+        );
+        return;
       }
       const representationFilter =
         typeof transportOptions.representationFilter === "string"
@@ -118,10 +124,7 @@ export default class ContentPreparer {
           video: context.initialVideoBitrate ?? 0,
         },
         lowLatencyMode: transportOptions.lowLatencyMode,
-        throttlers: {
-          limitResolution: { video: limitVideoResolution },
-          throttleBitrate: { video: throttleVideoBitrate },
-        },
+        throttlers,
       });
 
       const unbindRejectOnCancellation = currentMediaSourceCanceller.signal.register(
