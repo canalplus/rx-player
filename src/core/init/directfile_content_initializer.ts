65b309499fdf78c6fcc47a157fef6b1459fe498e
Florent Bouisset <florent.bouisset@gmail.com>
2025-07-08 11:36:20 +0200

    set autoplay attribute on media element so apple native HLS player correctly start bufferizing when setting autoplay true with the RxPlayer API

--- src/core/init/directfile_content_initializer.ts
+++ src/core/init/directfile_content_initializer.ts
@@ -89,6 +89,10 @@ export default class DirectFileContentInitializer extends ContentInitializer {
 
     clearElementSrc(mediaElement);
 
+    // Set the autoplay attribute on the mediaElement.
+    // On Apple devices, the native HLS player needs autoplay to be set
+    // in order to start buffering,which is required for our API's autoplay to work.
+    mediaElement.autoplay = this._settings.autoPlay;
     /**
      * Create dummy encryption data emitter, as those are not sent from the
      * RxPlayer for directfile contents.
