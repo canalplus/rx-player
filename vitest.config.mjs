3e4423738cac5ce654ecd96ea36e43d1a367f5b9
Paul Berberian <paul.berberian@proton.me>
2025-07-21 17:22:45 +0200

    Allow application to reset an audio/video track when fallbacking from all the others' one
    Based on #1715
    
    This commit shows an issue that can arise in the very rare scenario
    where:
    
    1. An application decides to load a content with no audio track (could
       be also with no video track).
    
    2. We fallback for some reason from all video tracks (or audio tracks
       if it's video that has been disabled in `1.`) - in the test here this
       is due to the impossibility to fetch their licenses.
    
    3. We should here receive a `noPlayableTrack` event indicating that no
       video track is playable, allowing us to re-set our audio choice to
       still be able to play something **BEFORE** the `NO_AUDIO_VIDEO_TRACKS`
       error that arises when no video nor audio is selected.
    
       However we don't here, we receive directly a `NO_AUDIO_VIDEO_TRACKS`
       error and stop playback.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -97,10 +97,7 @@ export default defineConfig({
     reporters: "dot",
     include: [
       // integration tests
-      "tests/integration/scenarios/**/*.[jt]s?(x)",
-      "tests/integration/**/*.test.[jt]s?(x)",
-      // memory tests
-      "tests/memory/**/*.[jt]s?(x)",
+      "tests/integration/scenarios/**/drm_base.[jt]s?(x)",
     ],
     globalSetup: "tests/globalSetup.mjs",
     browser: getBrowserConfig(process.env.BROWSER_CONFIG ?? "chrome"),
