43d78ff19881b656bff24011ac965d9bdbf86ff2
Paul Berberian <paul.berberian@proton.me>
2026-01-26 11:07:36 +0100

    tests: externalize generic code from our performance test runner
    Our performance tests, which tests for performance regressions on some
    key scenarios (loading a content, switching a track etc.) has its own
    testing library which:
    
    - Build a testing page both with the RxPlayer to test and the one to
      compare it to.
    
    - Find a path to a browser executable
    
    - Run the browser on those pages
    
    - Do performance measures, hundreds of time for each scenario
    
    - Compare the two and output the result
    
    Lately, I've also become very frustrated with `vitest`, the testing
    framework we use for other type of tests ("memory tests", integration
    tests and unit tests), and in that process made a generic very simple
    testing library on my side which only contains what we want to do:
    roughly the same steps than our performance tests without the
    performance part.
    
    When doing that work, I identified a lot of logic in the performance
    "run.mjs" script that could be useful as a separate script, specifically
    the logic to identify and run a browser on the current device.
    
    As that script is very large, I think this is a good idea to externalize
    that code even if the other test refactoring work is not merged.

---
+++ scripts/get_firefox_cmd.mjs
@@ -0,0 +1,134 @@
+#!/usr/bin/env node
+
+/**
+ * # get_firefox_cmd.mjs
+ *
+ * Utility to get the string to the Firefox browser's executable
+ * on your computer.
+ * It can be imported or called directly.
+ */
+
+import { execFile } from "child_process";
+import * as fsProm from "fs/promises";
+import * as path from "path";
+import { pathToFileURL } from "url";
+
+/**
+ * Returns string corresponding to the Firefox binary.
+ * @returns {Promise.<string|null>}
+ */
+export default async function getFirefoxCmd() {
+  switch (process.platform) {
+    case "win32": {
+      const suffix = ["Mozilla Firefox", "firefox.exe"];
+      const prefixes = [
+        process.env.PROGRAMFILES,
+        process.env["PROGRAMFILES(X86)"],
+        process.env.LOCALAPPDATA,
+      ];
+
+      for (const prefix of prefixes) {
+        if (!prefix) {
+          continue;
+        }
+        try {
+          const firefoxPath = path.join(prefix, ...suffix);
+          await fsProm.access(firefoxPath);
+          return firefoxPath;
+        } catch {}
+      }
+
+      return null;
+    }
+
+    case "darwin": {
+      const firefoxPath = "/Applications/Firefox.app/Contents/MacOS/firefox";
+      try {
+        await fsProm.access(firefoxPath);
+        return firefoxPath;
+      } catch {
+        return null;
+      }
+    }
+
+    case "linux": {
+      const absoluteCandidates = ["/usr/bin/firefox", "/snap/bin/firefox"];
+
+      for (const candidate of absoluteCandidates) {
+        try {
+          await fsProm.access(candidate);
+          return candidate;
+        } catch {}
+      }
+
+      try {
+        return await which("firefox");
+      } catch {
+        return null;
+      }
+    }
+
+    default:
+      throw new Error(`Error: unsupported platform: ${process.platform}`);
+  }
+}
+
+/**
+ * Execute `which` on the given command and output its output.
+ * @param {string} cmd
+ * @returns {Promise.<string>}
+ */
+function which(cmd) {
+  return new Promise((resolve, reject) => {
+    execFile("which", [cmd], (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout.split("\n")[0].trim());
+      }
+    });
+  });
+}
+
+// If true, this script is called directly
+if (process.argv[1] && import.meta.url === pathToFileURL(process.argv[1]).href) {
+  const args = process.argv.slice(2);
+  if (args.includes("-h") || args.includes("--help")) {
+    displayHelp();
+    process.exit(0);
+  }
+
+  getFirefoxCmd()
+    .then((firefoxCmd) => {
+      if (firefoxCmd) {
+        /* eslint-disable-next-line no-console */
+        console.log(firefoxCmd);
+      } else {
+        /* eslint-disable-next-line no-console */
+        console.error("No Firefox executable found on your machine");
+        process.exit(1);
+      }
+    })
+    .catch((err) => {
+      /* eslint-disable-next-line no-console */
+      console.error("Could not check the Firefox executable on your machine:", err);
+      process.exit(1);
+    });
+}
+
+/**
+ * Display through `console.log` a helping message relative to how to run this
+ * script.
+ */
+function displayHelp() {
+  /* eslint-disable-next-line no-console */
+  console.log(
+    `Returns path to the Firefox browser on your machine.
+Empty with a \`1\` exit code if not found.
+
+Usage: node get_firefox_cmd.mjs
+
+Available options:
+  -h, --help                 Display this help message`,
+  );
+}
---
+++ scripts/run_chrome.mjs
@@ -0,0 +1,208 @@
+#!/usr/bin/env node
+
+/**
+ * # run_chrome.mjs
+ *
+ * This utils allows to run the Chrome browser on a specific URL, in a headless
+ * mode or not.
+ * It can be imported or called directly, use `--help` flag for more info for
+ * the latter.
+ */
+
+import { spawn } from "child_process";
+import { pathToFileURL } from "url";
+import getChromeCmd from "../scripts/get_chrome_cmd.mjs";
+
+/** Flags used when starting the Chrome browser. */
+const CHROME_OPTIONS = [
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--disable-default-apps",
+  "--disable-extensions",
+  "--disable-popup-blocking",
+  "--disable-translate",
+  "--disable-device-discovery-notifications",
+  "--disk-cache-dir=/dev/null",
+
+  // "--disable-renderer-backgrounding",
+  // "--disable-background-timer-throttling",
+  // "--disable-gpu",
+  // "--disable-dev-shm-usage",
+];
+
+/**
+ * Flags specific to run Chrome in "headless mode" (without a UI).
+ * Useful for just running test or automation for example.
+ */
+const HEADLESS_OPTIONS = ["--headless"];
+
+/**
+ * Flags specific to disable the default Chrome's behavior of forbidding
+ * autoplay if the user did not interact with the page first.
+ */
+const AUTO_PLAY_OPTIONS = ["--autoplay-policy=no-user-gesture-required"];
+
+/**
+ * Flags allowing to expose more memory control and information.
+ * Useful for tests whose job is to detect memory leaks.
+ */
+const MEMORY_OPTIONS = ["--enable-precise-memory-info", "--js-flags=--expose-gc"];
+
+/**
+ * Run the Chrome browser on the given URL and return its process.
+ * @param {string} url - The URL to run on Chrome
+ * @param {Object} [params={}] - Launched Chrome's configuration
+ * @param {boolean|undefined} params.headless - If `true`, the browser will
+ * be launched in headless mode.
+ * @param {boolean|undefined} params.enableAutoPlay - If `true`, the browser
+ * will be launched with specific flags to disable auto-play blocking.
+ * Useful for tests.
+ * @param {boolean|undefined} params.memoryTools - If `true`, the browser
+ * will be launched with specific flags to enable precise memory info and the
+ * possibility to trigger JS heap GC by calling `window.gc`
+ * @param {boolean|undefined} params.verbose - If `true`, the browser will
+ * output to stdout. If not set or set to `false`, it will stay silent.
+ * Can be used to debug issues.
+ * @returns {Promise.<ChildProcess>}
+ */
+export default async function runChrome(
+  url,
+  { headless, enableAutoPlay, memoryTools, verbose } = {},
+) {
+  const chromeCmd = await getChromeCmd();
+  const flags = CHROME_OPTIONS;
+  if (headless) {
+    flags.push(...HEADLESS_OPTIONS);
+  }
+  if (enableAutoPlay) {
+    flags.push(...AUTO_PLAY_OPTIONS);
+  }
+  if (memoryTools) {
+    flags.push(...MEMORY_OPTIONS);
+  }
+  try {
+    const spawned = spawnProc(chromeCmd, [...flags, url], {
+      verbose,
+      parseError: (code) => "Failed to run Chrome. Code = " + code,
+    });
+    return spawned.child;
+  } catch (err) {
+    throw new Error("Could not launch page on Chrome: " + err.toString());
+  }
+}
+
+/**
+ * @param {string} command
+ * @param {Array.<string>} args
+ * @param {Object} [params]
+ * @param {Function|undefined} [params.parseError]
+ * @returns {Object}
+ */
+function spawnProc(command, args, { parseError, verbose } = {}) {
+  let child;
+  const prom = new Promise((res, rej) => {
+    child = spawn(command, args, { stdio: verbose ? "inherit" : undefined });
+    child.on("close", (code) => {
+      if (code) {
+        if (typeof parseError === "function") {
+          rej(parseError(code));
+        } else {
+          rej(code);
+        }
+        return;
+      }
+      res();
+    });
+  });
+  return {
+    promise: prom,
+    child,
+  };
+}
+
+// If true, this script is called directly
+if (process.argv[1] && import.meta.url === pathToFileURL(process.argv[1]).href) {
+  const urls = [];
+  let headless;
+  let enableAutoPlay;
+  let memoryTools;
+  let verbose;
+  const args = process.argv.slice(2);
+  for (let argOffset = 0; argOffset < args.length; argOffset++) {
+    const currentArg = args[argOffset];
+    switch (currentArg) {
+      case "-h":
+      case "--help":
+        /* eslint-disable no-fallthrough */
+        displayHelp();
+        process.exit(0);
+
+      case "--headless":
+        /* eslint-enable no-fallthrough */
+        headless = true;
+        break;
+
+      case "--enable-autoplay":
+        enableAutoPlay = true;
+        break;
+
+      case "--memory-tools":
+        memoryTools = true;
+        break;
+
+      case "--verbose":
+        verbose = true;
+        break;
+
+      case "--":
+        argOffset = args.length;
+        break;
+
+      default:
+        if (args[argOffset]) {
+          urls.push(args[argOffset]);
+        }
+        break;
+    }
+  }
+
+  if (urls.length === 0) {
+    console.error("Error: No URL provided");
+    displayHelp();
+    process.exit(1);
+  } else if (urls.length > 1) {
+    console.error("Error: Too many URLs provided:\n" + urls.join("\n"));
+    displayHelp();
+    process.exit(1);
+  }
+  runChrome(urls[0], {
+    headless,
+    enableAutoPlay,
+    memoryTools,
+    verbose,
+  }).catch((err) => {
+    /* eslint-disable-next-line no-console */
+    console.error("Could not run Chrome on the given URL:", err);
+    process.exit(1);
+  });
+}
+
+/**
+ * Display through `console.log` an helping message relative to how to run this
+ * script.
+ */
+function displayHelp() {
+  /* eslint-disable-next-line no-console */
+  console.log(
+    `Run the Chrome browser to the given URL.
+
+Usage: node run_chrome.mjs [options] <URL>
+
+Available options:
+  -h, --help           Display this help message
+  --headless           Start the browser in headless mode (without a UI)
+  --enable-autoplay    Allow autoplaying media even without user interaction
+  --memory-tools       Enable precize memory control in that browser
+  --verbose            Enable browser output to stdio`,
+  );
+}
